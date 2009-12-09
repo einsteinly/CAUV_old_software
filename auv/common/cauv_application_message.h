fa0ad91665ee9c9b8337468f7806a054608d1fa8
jesse <jesse@doonitz>
2009-12-09 17:56:58 +0000

    Added header files for Spread wrapper API

---
+++ auv/common/cauv_application_message.h
@@ -0,0 +1,21 @@
+#ifndef CAUV_APPLICATION_MESSAGE_H_INCLUDED
+#define CAUV_APPLICATION_MESSAGE_H_INCLUDED
+
+#include <vector>
+
+class ApplicationMessage {
+    virtual std::vector<char> getBytes() = 0;
+    virtual ApplicationMessage *deserialise(const std::vector<char> bytes) = 0;
+};
+
+/**
+ * This class is not currently implemented.
+ */
+class ScatterMessage : public ApplicationMessage {
+    virtual void addMessagePart(const ApplicationMessage &messagePart);
+    virtual void addMessagePart(const std::vector<char> &data);
+    virtual void addMessagePart(const void *const data, const int length);
+    short getPartsCount() const;
+};
+
+#endif // CAUV_APPLICATION_MESSAGE_H_INCLUDED
