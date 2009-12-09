d5e6d51238c1cda24375eb1ecdf52dfba0ee02ff
James Crosby <jc593@cam.ac.uk>
2009-12-09 22:30:05 +0000

    Add fileOutputNode.h (missed by previous commits)

---
+++ auv/img-pipeline/nodes/fileOutputNode.h
@@ -0,0 +1,54 @@
+#ifndef __FILE_OUTPUT_NODE_H__
+#define __FILE_OUTPUT_NODE_H__
+
+#include <map>
+#include <vector>
+#include <string>
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+#include "../node.h"
+#include "../image.h"
+
+
+class FileOutputNode: public Node{
+    public:
+        FileOutputNode(Scheduler& s)
+            : Node(s){
+            // one input:
+            registerInputID("image_in");
+            
+            // no outputs
+            // registerOutputID();
+            
+            // parameters: the filename, jpg compression, png compression
+            registerParamID<std::string>("filename", "default.out.jpg");
+            registerParamID<int>("jpeg quality", 95); // 0-100
+            registerParamID<int>("png compression", 9); // 0-9
+        }
+
+    protected:
+        out_image_map_t doWork(in_image_map_t& inputs){
+            out_image_map_t r;
+
+            std::cerr << "fileOutputNode::doWork" << std::endl;
+            
+            image_ptr_t img = inputs["image_in"];
+            std::string fname = param<std::string>("filename");
+            int jpg_qual = param<int>("jpeg quality");
+            int png_comp = param<int>("png compression");
+               
+            std::vector<int> imwrite_params;
+            imwrite_params.push_back(CV_IMWRITE_JPEG_QUALITY);
+            imwrite_params.push_back(jpg_qual);
+            imwrite_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+            imwrite_params.push_back(png_comp);
+
+            cv::imwrite(fname.c_str(), img->cvMat(), imwrite_params);
+            
+            return r;
+        }
+};
+
+#endif // ndef __FILE_OUTPUT_NODE_H__
