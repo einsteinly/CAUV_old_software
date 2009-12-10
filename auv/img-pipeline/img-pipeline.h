a50115e3ec7d7b01f4d867985a0acb07d1ac99f7
James Crosby <jc593@cam.ac.uk>
2009-12-10 11:53:51 +0000

    Add image pipeline cauv node (img-pipeline), doesn\'t do anything yet.

---
+++ auv/img-pipeline/img-pipeline.h
@@ -0,0 +1,18 @@
+#ifndef __IMAGE_PIPELINE_H__
+#define __IMAGE_PIPELINE_H__
+
+#include <common/cauv_node.h>
+
+
+class ImagePipelineNode : public CauvNode
+{
+    public:
+        ImagePipelineNode(const string& group);
+        virtual ~ImagePipelineNode();
+    
+    protected:
+        virtual void onRun();
+};
+
+#endif //__IMAGE_PIPELINE_H__
+
