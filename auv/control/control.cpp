89110b5e051d14c0fc4a9f2e81a699d822d30a2f
Leszek Swirski <ls426@cam.ac.uk>
2010-03-16 15:34:39 +0000

    * Made module templated, with mcb module as a typedef * Added msb module to control

--- auv/control/control.cpp
+++ auv/control/control.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <sstream>
 
+#include <boost/make_shared.hpp>
+
 #include <common/cauv_global.h>
 #include <common/cauv_utils.h>
 #include <common/messages.h>
@@ -12,12 +14,12 @@ using namespace std;
 
 ControlNode::ControlNode(const string& group) : CauvNode("Control", group)
 {
-    /*/ start up the INS module
+    // start up the MCB module
     try {
-        m_ins = new InsModule(0);
+        m_mcb = boost::make_shared<MCBModule>(0);
         //m_ins->addObserver(m_state_updating_observer);
         //m_ins->addObserver(new PrintingModuleObserver());
-        cauv_global::trace("INS Connected");
+        cauv_global::trace("MCB Connected");
 
         // create a thread to send the alive messages
         // the alive messages keep the motors rom being automatically killed by the motor control board
@@ -28,13 +30,15 @@ ControlNode::ControlNode(const string& group) : CauvNode("Control", group)
         //    exit(-1);
         //}
     } catch (FTDIException& e) {
-        cauv_global::error("Cannot connect to INS", e);
-        m_ins = 0;
-    }*/
+        cauv_global::error("Cannot connect to MCB", e);
+        m_mcb.reset();
+    }
 
     // start up the Xsens IMU
     try {
-        m_xsens = new XsensIMU(0);
+        m_xsens = boost::make_shared<XsensIMU>(0);
+        cauv_global::trace("XSens Connected");
+        
         CmtOutputMode om = CMT_OUTPUTMODE_CALIB | CMT_OUTPUTMODE_ORIENT;
         CmtOutputSettings os = CMT_OUTPUTSETTINGS_ORIENTMODE_EULER | CMT_OUTPUTSETTINGS_DATAFORMAT_FLOAT;
 
@@ -47,14 +51,8 @@ ControlNode::ControlNode(const string& group) : CauvNode("Control", group)
         m_xsens->configure(om, os);
     } catch (XsensException e) {
         cauv_global::error("Cannot connect to Xsens", e);
-        m_xsens = 0;
+        m_xsens.reset();
     }
-}
-
-ControlNode::~ControlNode()
-{
-	//delete m_ins;
-	delete m_xsens;
 }
 
 
@@ -63,22 +61,16 @@ void ControlNode::onRun()
     CauvNode::onRun();
 
     // This is the forwarding thread
-    // It takes the Xsens output and pushes it to the CAUV INS if connected
-    // It also forwards the Xsens output to the telemetry state
+    // It forwards the Xsens output to the telemetry state
     
-    // First check the Xsens and CAUV INS are actually connected...
+    // First check the Xsens is actually connected...
     if (!m_xsens) {
-        stringstream message;
-        message << "Xsens must be connected. Killing forwarding thread." << endl;
-        message << "  INS Connected:   " << /*(m_ins ? 1 : 0)*/ 0 << endl;
-        message << "  Xsens Connected: " << (m_xsens ? 1 : 0) << endl;
-        cauv_global::error(message.str());
+        cauv_global::error("Xsens must be connected. Killing forwarding thread.");
         return;
     }
 
     while (true) {
         floatYPR att = m_xsens->getAttitude();
-        //cout << fixed << "Yaw: " << att.yaw << " Pitch: " << att.pitch  << " Roll: " << att.roll  << endl;
 
         //update the telemetry state
         att.yaw = -att.yaw;
@@ -86,7 +78,7 @@ void ControlNode::onRun()
             att.yaw += 360;
 
         cout << fixed << "Yaw: " << att.yaw << " Pitch: " << att.pitch  << " Roll: " << att.roll  << endl;
-        
+
 	    msleep(10);
     }
 }
--- auv/module/module.h
+++ auv/module/module.h
@@ -5,10 +5,15 @@
 #include <iostream>
 #include <string.h>
 #include <sstream>
+
 #include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
+#include <boost/iostreams/stream.hpp>
 #include <boost/iostreams/concepts.hpp>
 #include <boost/thread.hpp>
 
+#include <common/cauv_utils.h>
+#include <common/cauv_global.h>
 #include <common/messages.h>
 
 class FTDIException : public std::exception
@@ -22,41 +27,210 @@ class FTDIException : public std::exception
         virtual const char* what() const throw();
 };
 
+template<int baudrate, ftdi_bits_type bits, ftdi_stopbits_type stopBits, ftdi_parity_type parity, int flowControl>
 class FTDIStream : public boost::iostreams::device<boost::iostreams::bidirectional>
 {
     public:
-        FTDIStream() throw();
-        FTDIStream(int deviceID) throw(FTDIException);
-    
-        void baudrate(int baudrate) throw(FTDIException);
-        void lineProperty(ftdi_bits_type bits, ftdi_stopbits_type stopBits, ftdi_parity_type parity) throw(FTDIException);
-        void flowControl(int flowControl) throw(FTDIException);
+        FTDIStream(int deviceID) throw(FTDIException)
+        {
+            ftdi_init(&ftdic);
 
-        std::streamsize read(char* s, std::streamsize n);
-        std::streamsize write(const char* s, std::streamsize n);
-        void close();
-        void close(std::ios_base::openmode which);
+            struct ftdi_device_list* devices,* curdevices;
+            int ret;
+
+            if ((ret = ftdi_usb_find_all(&ftdic, &devices, 0x0403, 0x6001)) < 0)
+            {
+                throw FTDIException("ftdi_usb_find_all failed", ret, &ftdic);
+            }
+
+            int deviceCount = ret;
+
+            cauv_global::trace(MakeString() << "Number of devices found: " << deviceCount);
+
+            if (deviceCount == 0)
+                throw FTDIException("Could not open device (No devices found)");
+
+            curdevices = devices;
+            int i = deviceID;
+            while (i--)
+            {
+                if (curdevices == 0)
+                {
+                    throw FTDIException("Could not open device (No matching device found)");
+                }
+                curdevices = curdevices->next;
+            }
+
+            boost::scoped_ptr<struct usb_device> device(curdevices->dev);
+
+            ftdi_list_free(&devices);
+
+            cauv_global::trace(MakeString() << "Module's device (id:" << deviceID << ") found...");
+
+            if ((ret = ftdi_usb_open_dev(&ftdic, device.get())) < 0)
+            {
+                throw FTDIException("Unable to open ftdi device", ret, &ftdic);
+            }
+            
+            // Set properties
+            if ((ret = ftdi_set_baudrate(&ftdic, baudrate)) < 0)
+            {
+                throw FTDIException("Unable to set baudrate", ret, &ftdic);
+            }
+            if ((ret = ftdi_set_line_property(&ftdic, bits, stopBits, parity)) < 0)
+            {
+                throw FTDIException("Unable to set line property", ret, &ftdic);
+            }
+            if ((ret = ftdi_setflowctrl(&ftdic, flowControl)) < 0)
+            {
+                throw FTDIException("Unable to set flow control", ret, &ftdic);
+            }
+        }
+        
+        
+        std::streamsize read(char* s, std::streamsize n)
+        {
+            int ret = ftdi_read_data(&ftdic, reinterpret_cast<u_char*>(s), n);
+            if (ret < 0)
+            {
+                throw FTDIException("Unable to read from ftdi device", ret, &ftdic);
+            }
+            return ret;
+        }
+
+        std::streamsize write(const char* s, std::streamsize n)
+        {
+            int ret = ftdi_write_data(&ftdic, const_cast<u_char*>(reinterpret_cast<const u_char*>(s)), n);
+            if (ret < 0)
+            {
+                throw FTDIException("unable to write to ftdi device", ret, &ftdic);
+            }
+            return ret;
+        }
+        
+        void close() 
+        {
+            ftdi_usb_close(&ftdic);
+            ftdi_deinit(&ftdic);
+        }
+
+        void close(std::ios_base::openmode which) 
+        {
+            this->close();
+        } 
 
     protected:
         struct ftdi_context ftdic;
 };
 
+
+template<int baudrate, ftdi_bits_type bits, ftdi_stopbits_type stopBits, ftdi_parity_type parity, int flowControl>
 class Module : public MessageSource
 {
-	public:
-        ~Module();
-        void send(Message& message) throw(FTDIException);
+    typedef FTDIStream<baudrate, bits, stopBits, parity, flowControl> ftdi_stream_t;
     
+    public:
+        Module(int deviceID) throw(FTDIException) : ftdiStream(deviceID)
+        {
+            cauv_global::trace("USB opened...");
+
+            m_running = true;
+            m_readThread = boost::make_shared<boost::thread>(&Module::readLoop, this);
+
+            cauv_global::trace("Module thread started");
+        }
+
+        ~Module()
+        {
+            m_running = false;
+            m_readThread->join();
+
+            ftdiStream.close();
+        }
+
+
+        void send(Message& message) throw (FTDIException)
+        {
+            boost::iostreams::stream<ftdi_stream_t> ftdi(ftdiStream);
+
+            byte_vec_t bytes = message.toBytes();
+
+
+            uint32_t buf[2];
+            buf[0] = 0xdeadc01d;
+            buf[1] = bytes.size();
+            uint16_t* buf16 = reinterpret_cast<uint16_t*>(buf); 
+            std::vector<uint16_t> vheader(buf16, buf16+4);
+            uint16_t checksum = sumOnesComplement(vheader);
+
+
+            ftdi << 0xdeadc01d;
+            ftdi << bytes.size();
+            ftdi << checksum;
+            ftdi << bytes;
+        }
+
     protected:
-        FTDIStream ftdiStream;
+        ftdi_stream_t ftdiStream;
         boost::shared_ptr<boost::thread> m_readThread;
         volatile bool m_running;
 
-        Module(int deviceID, int baudrate, ftdi_bits_type bits, ftdi_stopbits_type stopBits, ftdi_parity_type parity, int flowControl) throw(FTDIException);
-    
-        void readLoop();
+        void readLoop()
+        {
+            byte_vec_t curMsg;
+
+            boost::iostreams::stream<ftdi_stream_t> ftdi(ftdiStream);
+
+            while (m_running)
+            {
+                uint32_t startWord;
+                ftdi >> startWord;
+
+                if (startWord != 0xdeadc01d)
+                {
+                    // Start word doesn't match, drop it
+                    continue;
+                }
+
+                uint32_t len;
+                uint16_t checksum;
+                ftdi >> len;
+                ftdi >> checksum;
+
+                uint32_t buf[2];
+                buf[0] = startWord;
+                buf[1] = len;
+
+                uint16_t* buf16 = reinterpret_cast<uint16_t*>(buf); 
+                std::vector<uint16_t> vheader(buf16, buf16+4);
+
+                if (sumOnesComplement(vheader) != checksum)
+                {
+                    // Checksum doesn't match, drop it
+                    continue;
+                }
+
+                curMsg.clear();
+                char c;
+                for (uint32_t i = 0; i < len; ++i)
+                {
+                    ftdi >> c;
+                    curMsg.push_back(c);
+                }
+
+                std::cout << "Module Message  [ len: " << len << " | checksum: " << checksum << " ]" << std::endl;
+                foreach(char c, curMsg)
+                {
+                    std::cout << std::hex << std::setw(2) << std::setfill('0') << c << " " << std::dec;
+                }
+                std::cout << std::endl;
+
+                this->notifyObservers(curMsg);
+            }
+        }
 };
 
-void readDevice(Module* module);
+
+typedef Module<38400, BITS_8, STOP_BIT_1, NONE, SIO_DISABLE_FLOW_CTRL> MCBModule;
 
 #endif // __MODULE_H__
