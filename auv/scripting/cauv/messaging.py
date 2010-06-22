060105dba561b6c9e5ab7fe3467fd08737020ffe
James Crosby <jc593@cam.ac.uk>
2010-06-22 16:04:05 +0100

    python module now relatively nicely integrated --- still lots of absolute paths in the Jamfile though

--- auv/scripting/cauv/messaging.py
+++ auv/scripting/cauv/messaging.py
@@ -1,8 +1,5 @@
 # This module transparently wrapped the boost-python exported messaging
 # interafaace
-import sys
-_saved = sys.path
-sys.path = [r'/home/jc593/Dev/hg-code/auv/scripting/bin/gcc-4.2.4/debug/'] + sys.path
+
 from cauvinterface import *
-sys.path = _saved
 
