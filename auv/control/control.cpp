a07b6e18cf069481204e87b5b89f1d1284c59ecc
James Crosby <jc593@cam.ac.uk>
2010-06-28 18:27:22 +0200

    oops, fix manual override

--- auv/control/control.cpp
+++ auv/control/control.cpp
@@ -222,7 +222,17 @@ class ControlLoops : public MessageObserver, public XsensObserver
         virtual void onMotorMessage(MotorMessage_ptr m)
         {
             debug(2) << "Set manual motor demand based on motor message:" << *m;
-            m_mcb->send(m);
+            
+            switch(m->MotorID)
+            {
+                case MotorID::Prop: m_demand[ManualOverride].prop = m->speed;
+                case MotorID::HBow: m_demand[ManualOverride].hbow = m->speed;
+                case MotorID::HStern: m_demand[ManualOverride].hstern = m->speed;
+                case MotorID::VBow: m_demand[ManualOverride].vbow = m->speed;
+                case MotorID::VStern: m_demand[ManualOverride].vstern = m->speed;
+            }
+            
+            updateMotorControl();
         }
 
         
