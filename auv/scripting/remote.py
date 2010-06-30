f54c639186f7d00fba0b87a7c9f192ce63313174
James Crosby <jc593@cam.ac.uk>
2010-06-30 14:36:40 +0200

    python remote-scripting

--- auv/scripting/remote.py
+++ auv/scripting/remote.py
@@ -8,49 +8,57 @@ import threading
 import copy
 import traceback
 import time
+import Queue
 
 class ScriptObserver(msg.BufferedMessageObserver, threading.Thread):
     def __init__(self, node):
         msg.BufferedMessageObserver.__init__(self)
         threading.Thread.__init__(self)
         self.__node = node
+        node.join("control")
         node.addObserver(self)
-        self.eval_context_locals = {}
-        self.eval_context_globals = {"msg": self.sendScriptResponse}
-        self.eval_queue = []
-        self.eval_queue_condition = threading.Condition()
+        self.resetEnvironment()
+        self.eval_queue = Queue.Queue(1000)
         # TODO: check this line:
         self.daemon = True
         self.start()
+
+    def resetEnvironment(self):
+        self.eval_context_locals = {}
+        self.eval_context_globals = {"response": self.sendScriptResponse}
 
     def send(self, m):
         self.__node.send(m, "gui")
     
     def onScriptMessage(self, m):
         print 'received script:\n%s\n#[end script]' % m.script
-        self.eval_queue.append(m.script, m.timeout)
+        self.eval_queue.put((m.script, m.timeout))
 
-    def sendScriptResponse(self, msg):
-        self.send(msg.ScriptResponseMessage(msg))
+    def sendScriptResponse(self, m):
+        print 'sending script response:', m
+        self.send(msg.ScriptResponseMessage(m))
 
     def run(self):
         print 'script queue thread started'
         while True:
-            script = None
-            self.eval_queue_condition.acquire()
-            self.eval_queue_condition.wait()
-            script = self.eval_queue.pop()
-            self.eval_queue_condition.release()
-            evaluate(script)
+            (script, timeout) = self.eval_queue.get()
+            self.evaluate(script, timeout)
 
-    def evaluate(self, script):
-        print 'evaluating script\n%s\n#[end script]' % m.script
+    def evaluate(self, script, timout):
+        # TODO: apply timeout... somehow
+        print 'evaluating script\n%s\n#[end script]' % script
+        with open('remote.py.script.tmp', 'w+') as tmpf:
+            tmpf.write(script)
         try:
-            r = eval(script, self.eval_context_globals, self.eval_context_globals)
+            r = execfile('remote.py.script.tmp',
+                         self.eval_context_globals,
+                         self.eval_context_globals)
         except Exception:
             print 'error:', traceback.format_exc()
+            self.sendScriptResponse('error:' + traceback.format_exc())
         else:
-            print 'script returned:', r
+            print 'script returned:', str(r)
+            self.sendScriptResponse('script returned:' + str(r))            
 
 def main():
     n = node.Node("pyscript")
