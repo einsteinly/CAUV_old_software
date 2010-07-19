8017680e56e6db42c1ae965b0627ebfc765b685f
James Crosby <jc593@cam.ac.uk>
2010-07-19 08:04:37 +0100

    python stuff working on OS X

--- auv/scripting/start.sh
+++ auv/scripting/start.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-SCRIPTING_DIR=$(dirname $(readlink -f $0))
+SCRIPTING_DIR="$(pwd)/cauv"
+echo "Scripting dir is: $SCRIPTING_DIR"
 
 export LD_LIBRARY_PATH="$SCRIPTING_DIR/cauv/:$LD_LIBRARY_PATH"
+export DYLD_LIBRARY_PATH="$SCRIPTING_DIR/cauv/:$DYLD_LIBRARY_PATH"
 export PYTHON_PATH="$SCRIPTING_DIR:$PYTHONPATH"
+
+echo "LD path: $LD_LIBRARY_PATH"
+echo "DYLD path: $DYLD_LIBRARY_PATH"
 
 python -i -c "import cauv
 import cauv.messaging as msg
