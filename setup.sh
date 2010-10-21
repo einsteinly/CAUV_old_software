20da637a24ac98fe9d1597d6b5d776d07d0e1d0d
James Crosby <jc593@cam.ac.uk>
2010-10-21 16:29:38 +0100

    add setup script: creats an hgrc with user name / real name, trys to ensure umask is set to 002 in ~/.profile

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+set -e
+
+if [ -a ~/.hgrc ]; then
+    echo "not creating ~/.hgrc (already exists)"
+else
+    echo "creating ~/.hgrc"
+    # add user name and user id strings
+    whoami | xargs getent passwd | sed "s/\([^:]\+\):[x:0 -9]*:\([^:,]*\).*/[ui]\nusername = \1 <\2>\n/" >> ~/.hgrc
+fi
+
+if grep -q "^[[:space:]]*umask[[:space:]]\+002" ~/.profile; then
+    echo "umask already set correctly in ~/.profile"
+elif grep -q "^[[:space:]]*umask[[:space:]]\+022" ~/.profile; then
+    echo "changing umask setting in ~/.profile"
+    sed -i.cauv-saved "s/^\([[:space:]]*\)umask[[:space:]]\+022/\1umask 002/" ~/.profile
+else
+    echo "adding umask setting to ~/.profile"
+    echo "umask 002" >> ~/.profile
+fi
+
+
+
+
+
