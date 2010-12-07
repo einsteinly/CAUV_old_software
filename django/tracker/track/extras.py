ae916ec36863f4e52aef180e8a463faea8164ee3
Jason Wyatt <jw682@cam.ac.uk>
2010-12-07 13:38:02 +0000

    Added users to pitz web app. fixed some web bugs

--- django/tracker/track/extras.py
+++ django/tracker/track/extras.py
@@ -1,10 +1,11 @@
+known_pitz_fields = ('modified_time','created_time','frag','type','yaml_file_saved','uuid' )
 def get_all_variables(entity_type):
     fields = {}
     try:
         for variable in entity_type.all()[0]: # get the name/type of variables in the first member of the bag
-            fields[variable] = type(entity_type.all()[0][variable])
+	    if not variable in known_pitz_fields:
+                fields[variable] = type(entity_type.all()[0][variable])
     except IndexError:
         pass
     fields.update(entity_type.allowed_types)
-    print fields
     return fields
