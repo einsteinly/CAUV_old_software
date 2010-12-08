be8c73a485b0fc7b6d04c96570f86d1ef988ecc1
James Crosby <jc593@cam.ac.uk>
2010-12-08 01:44:55 +0000

    Python fixes for OS X.. finally!

--- auv/scripting/run.sh
+++ auv/scripting/run.sh
@@ -3,8 +3,8 @@
 SCRIPTING_DIR="$(pwd)"
 #echo "Scripting dir is: $SCRIPTING_DIR"
 
-export LD_LIBRARY_PATH="$SCRIPTING_DIR/cauv/:$LD_LIBRARY_PATH"
-export DYLD_LIBRARY_PATH="$SCRIPTING_DIR/cauv/:$DYLD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$SCRIPTING_DIR/../python-module/cauv/:$LD_LIBRARY_PATH"
+export DYLD_LIBRARY_PATH="$SCRIPTING_DIR/../python-module/cauv/:$DYLD_LIBRARY_PATH"
 export PYTHONPATH="$SCRIPTING_DIR:$SCRIPTING_DIR/../python-module/:$PYTHONPATH"
 
 #echo "LD path: $LD_LIBRARY_PATH"
--- auv/scripting/start.sh
+++ auv/scripting/start.sh
@@ -3,14 +3,14 @@
 SCRIPTING_DIR="$(pwd)"
 #echo "Scripting dir is: $SCRIPTING_DIR"
 
-export LD_LIBRARY_PATH="$SCRIPTING_DIR/cauv/:$LD_LIBRARY_PATH"
-export DYLD_LIBRARY_PATH="$SCRIPTING_DIR/cauv/:$DYLD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$SCRIPTING_DIR/../python-module/cauv/:$LD_LIBRARY_PATH"
+export DYLD_LIBRARY_PATH="$SCRIPTING_DIR/../python-module/cauv/:$DYLD_LIBRARY_PATH"
 export PYTHONPATH="$SCRIPTING_DIR:$SCRIPTING_DIR/../python-module/:$PYTHONPATH"
 
 #echo "LD path: $LD_LIBRARY_PATH"
 #echo "DYLD path: $DYLD_LIBRARY_PATH"
 
-gdb --args python -i -c "import cauv
+python -i -c "import cauv
 import cauv.messaging as msg
 import cauv.pipeline as pipeline
 import cauv.control as control
