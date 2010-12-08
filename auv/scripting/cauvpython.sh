61d85d351f93e60496b325bfbd49837eb5103c61
James Crosby <jc593@cam.ac.uk>
2010-12-08 02:08:41 +0000

    More OS X Python fixes

--- auv/scripting/cauvpython.sh
+++ auv/scripting/cauvpython.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 
-SCRIPTING_DIR="$(pwd)"
-#echo "Scripting dir is: $SCRIPTING_DIR"
-
-export LD_LIBRARY_PATH="$SCRIPTING_DIR/cauv/:$LD_LIBRARY_PATH"
-export DYLD_LIBRARY_PATH="$SCRIPTING_DIR/cauv/:$DYLD_LIBRARY_PATH"
-export PYTHONPATH="$SCRIPTING_DIR:$SCRIPTING_DIR/../python-module/:$PYTHONPATH"
-
-#echo "LD path: $LD_LIBRARY_PATH"
-#echo "DYLD path: $DYLD_LIBRARY_PATH"
-
-python -i
+./run.sh $@
 
--- auv/scripting/run.sh
+++ auv/scripting/run.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 SCRIPTING_DIR="$(pwd)"
-#echo "Scripting dir is: $SCRIPTING_DIR"
+LD_EXT_PATH="$SCRIPTING_DIR/../python-module/cauv/:$SCRIPTING_DIR/../python-module/"
 
-export LD_LIBRARY_PATH="$SCRIPTING_DIR/../python-module/cauv/:$LD_LIBRARY_PATH"
-export DYLD_LIBRARY_PATH="$SCRIPTING_DIR/../python-module/cauv/:$DYLD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$LD_EXT_PATH:$LD_LIBRARY_PATH"
+export DYLD_LIBRARY_PATH="$LD_EXT_PATH:$DYLD_LIBRARY_PATH"
 export PYTHONPATH="$SCRIPTING_DIR:$SCRIPTING_DIR/../python-module/:$PYTHONPATH"
 
-#echo "LD path: $LD_LIBRARY_PATH"
-#echo "DYLD path: $DYLD_LIBRARY_PATH"
+echo $DYLD_LIBRARY_PATH
 
 python $@
 
