960ac835df310102f573f75f7f374ad8b8cada36
Leszek Swirski <ls426@cam.ac.uk>
2010-12-09 18:56:18 +0000

    Fixed OIS error

--- cmake/FindOIS.cmake
+++ cmake/FindOIS.cmake
@@ -1,49 +1,38 @@
-#-------------------------------------------------------------------
-# This file is part of the CMake build system for OGRE
-#     (Object-oriented Graphics Rendering Engine)
-# For the latest info, see http://www.ogre3d.org/
-#
-# The contents of this file are placed in the public domain. Feel
-# free to make use of it in any way you like.
-#-------------------------------------------------------------------
-
 # - Try to find OIS
 # Once done, this will define
 #
 #  OIS_FOUND - system has OIS
-#  OIS_INCLUDE_DIRS - the OIS include directories 
+#  OIS_INCLUDE_DIRS - the OIS include directories
 #  OIS_LIBRARIES - link these to use OIS
 
-include(FindPkgMacros)
-findpkg_begin(OIS)
+include(LibFindMacros)
 
-# Get path, convert backslashes as ${ENV_${var}}
-getenv_path(OIS_HOME)
-getenv_path(OGRE_CTHUGHA)
+# Use pkg-config to get hints about paths
+libfind_pkg_check_modules(OIS_PKGCONF ois) 
 
-# construct search paths
-set(OIS_PREFIX_PATH ${OIS_HOME} ${ENV_OIS_HOME} 
-  ${OGRE_CTHUGHA}/Dependencies ${ENV_OGRE_CTHUGHA}/Dependencies)
-create_search_paths(OIS)
-# redo search if prefix path changed
-clear_if_changed(OIS_PREFIX_PATH
-  OIS_LIBRARY_FWK
-  OIS_LIBRARY_REL
-  OIS_LIBRARY_DBG
-  OIS_INCLUDE_DIR
+# Include dir
+find_path(OIS_INCLUDE_DIR
+  NAMES OIS
+  PATHS ${OIS_PKGCONF_INCLUDE_DIRS}
+)
+#message ("OIS_PKGCONF_INCLUDE_DIRS=${OIS_PKGCONF_INCLUDE_DIRS}")
+#message ("OIS_INCLUDE_DIR=${OIS_INCLUDE_DIR}")
+
+# Finally the library itself
+find_library(OIS_LIBRARY
+  NAMES OIS
+  PATHS ${OIS_PKGCONF_LIBRARY_DIRS}
 )
 
-set(OIS_LIBRARY_NAMES OIS)
-get_debug_names(OIS_LIBRARY_NAMES)
+# Set the include dir variables and the libraries and let libfind_process do the rest.
+# NOTE: Singular variables for this library, plural for libraries this this lib depends on.
+set(OIS_PROCESS_INCLUDES
+    OIS_INCLUDE_DIR)
+set(OIS_PROCESS_LIBS
+    OIS_LIBRARY)
+libfind_process(OIS)
 
-use_pkgconfig(OIS_PKGC OIS)
+#message ("OIS_INCLUDE_DIRS=${OIS_INCLUDE_DIRS}")
+#message ("OIS_LIBRARIES=${OIS_LIBRARIES}")
 
-findpkg_framework(OIS)
 
-find_path(OIS_INCLUDE_DIR NAMES OIS.h HINTS ${OIS_INC_SEARCH_PATH} ${OIS_PKGC_INCLUDE_DIRS} PATH_SUFFIXES OIS)
-find_library(OIS_LIBRARY_REL NAMES ${OIS_LIBRARY_NAMES} HINTS ${OIS_LIB_SEARCH_PATH} ${OIS_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" release relwithdebinfo minsizerel)
-find_library(OIS_LIBRARY_DBG NAMES ${OIS_LIBRARY_NAMES_DBG} HINTS ${OIS_LIB_SEARCH_PATH} ${OIS_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" debug)
-make_library_set(OIS_LIBRARY)
-
-findpkg_finish(OIS)
-add_parent_dir(OIS_INCLUDE_DIRS OIS_INCLUDE_DIR)
