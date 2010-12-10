d4084bf2c825ede040c2ab62e403bb66217a7cd6
Daniel Kalotay <dk362@cam.ac.uk>
2010-12-10 17:02:32 +0000

    added CopyNodeMask

--- auv/img-pipeline/nodes/nodes.cpp
+++ auv/img-pipeline/nodes/nodes.cpp
@@ -38,6 +38,7 @@
 #include "quickSegmentNode.h"
 #include "thresholdMaskNode.h"
 #include "histogramSegmentationNodeArb.h"
+#include "copyNodeMask.h"
 
 // Register node types (actually definitions of static data members)
 DEFINE_NFR(CopyNode, NodeType::Copy);
@@ -78,6 +79,7 @@ DEFINE_NFR(CentreFinderNode, NodeType::Centre);
 DEFINE_NFR(QuickSegmentNode, NodeType::QuickSegment);
 DEFINE_NFR(ThresholdMaskNode, NodeType::ThresholdMask);
 DEFINE_NFR(HistogramSegmentationNodeArb, NodeType::HistogramSegmentationArb);
+DEFINE_NFR(CopyNodeMask, NodeType::CopyMask);
 
 boost::try_mutex CameraInputNode::m_capture_lock[MAX_DEVICES];
 
