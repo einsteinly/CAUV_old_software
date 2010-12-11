e9002457c62e26dc50121432a00180dafb3f39ef
James Crosby <jc593@cam.ac.uk>
2010-12-11 14:37:51 +0000

    new motor test, with optparse

--- auv/scripting/motortest.py
+++ auv/scripting/motortest.py
@@ -8,87 +8,108 @@ import cauv.node
 
 import time
 import traceback
+import optparse
 
 
-def motorTest(): 
-    node = cauv.node.Node('py-mt')
-    auv = control.AUV(node)
-    
-    print 'setting calibration...'
+def setupParams(node, auv):
+    print 'Setting calibration parameters'
     # set-up calibration factors
     node.send(msg.DepthCalibrationMessage(
         -912.2/96.2, 1.0/96.2, -912.2/96.2, 1.0/96.2
     ), "control")
 
-    #auv.bearingParams(1, 0, -80, 1)
-    #auv.depthParams(-40, 0, 0, 1)
-    #auv.pitchParams(1, 0, 0, 1)
+    auv.bearingParams(1, 0, -80, 1)
+    auv.depthParams(-40, 0, 0, 1)
+    auv.pitchParams(1, 0, 0, 1)
 
     auv.propMap(10, -10, 127, -127)
     auv.vbowMap(10, -10, 127, -127)
     auv.hbowMap(10, -10, 127, -127)
     auv.vsternMap(10, -10, 127, -127)
     auv.hsternMap(10, -10, 127, -127)
-    
-    delay = 5
-    power = 20
+
+
+def motorTest(node, auv, power=30, delay=3, quiet = False):
+    def qprint(s):
+        if not quiet:
+            print s
 
     auv.stop()
-    print 'prop forwards:'
+    qprint('prop forwards:')
     auv.prop(power)
     time.sleep(delay)
 
     auv.stop()
-    print 'prop reverse:'
+    qprint('prop reverse:')
     auv.prop(-power)
     time.sleep(delay)
 
     auv.stop()
-    print 'hbow right:'
+    qprint('hbow right:')
     auv.hbow(power)
     time.sleep(delay)
     auv.stop()
     
     auv.stop()
-    print 'hbow left:'
+    qprint('hbow left:')
     auv.hbow(-power)
     time.sleep(delay)
 
     auv.stop()
-    print 'vbow up:'
+    qprint('vbow up:')
     auv.vbow(power)
     time.sleep(delay)
     
     auv.stop()
-    print 'vbow down:'
+    qprint('vbow down:')
     auv.vbow(-power)
     time.sleep(delay)
 
     auv.stop()
-    print 'hstern right:'
+    qprint('hstern right:')
     auv.hstern(power)
     time.sleep(delay)
     
     auv.stop()
-    print 'hstern left:'
+    qprint('hstern left:')
     auv.hstern(-power)
     time.sleep(delay)
 
     auv.stop()
-    print 'vstern up:'
+    qprint('vstern up:')
     auv.vstern(power)
     time.sleep(delay)
     
     auv.stop()
-    print 'vstern down:'
+    qprint('vstern down:')
     auv.vstern(-power)
     time.sleep(delay)
 
     auv.stop()
-    print 'Complete'
+    qprint('Complete')
 
 if __name__ == '__main__':
-    motorTest()
+    p = optparse.OptionParser()
+    p.add_option("-q", "--quiet", dest="quiet", default=False,
+            action="store_true", help="don't print progress")
+    p.add_option("-d", "--delay", dest="delay", type=float, default=3.0,
+            help="pause after each motor command")
+    p.add_option("-p", "--power", dest="power", help=" motor power", type=int,
+            default=30)
+    p.add_option("-N", "--no-params", dest="no_params", default=False,
+            action="store_true",
+            help="don't set motor maps and other parameters")
 
+    p.add_option("-n", "--name", dest="name", default="py-my",
+            help="CAUV Node name")
+    
+    opts, args = p.parse_args()
+    
+    node = cauv.node.Node('py-mt')
+    auv = control.AUV(node)
 
+    if not opts.no_params:
+        setupParams(node, auv)
+
+    motorTest(node, auv, opts.power, opts.delay)
 
