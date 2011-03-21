62344c571ef011a2bb6502642068bedae695dca9
Jason Wyatt <devnull@localhost>
2011-03-21 23:40:42 +0000

    Python battery monitor update for new motor messages

--- auv/scripting/battery_monitor.py
+++ auv/scripting/battery_monitor.py
@@ -3,8 +3,8 @@ import cauv.messaging as messaging
 import cauv.node
 
 import threading
-import logging
 import time
+import optparse
 
 log_file = 'bat_log.log'
 
@@ -13,70 +13,104 @@ lights_consumption = 32#Watts
 computer_consumption = 30#Watts 1.5 amps at 19 volts
 constant_power_use = lights_consumption+computer_consumption #Watts
 battery_total = 89.1*2 #Watt hours
-#
-def estimatePowerUse(motor_power_list, time):
-    try:
-        return ((sum(motor_power_list)/len(motor_power_list))*motor_conversion_factor+constant_power_use)*time/3600
-    except ZeroDivisionError:
-        print "Error, no data"
-        return 0
 
+#function to estimate power use
+def estimateMotorPowerUse(motor_log):
+    power_used = 0
+    for motor in motor_log:
+        try:
+            for x in range(len(motor_log[motor])-1):
+                power_used += (motor_log[motor][x+1][1]+motor_log[motor][x][1])/(motor_log[motor][x+1][0]-motor_log[motor][x][0])
+        except IndexError:
+            print "Error, no data for "+motor
+    return power_used
+        
+def estimateConstantPowerUse(interval):
+    return constant_power_use*interval/3600
+
+motor_ids=['prop','vbow','hbow','vstern','hstern']
+#message logger
 class motorStateLogger(messaging.BufferedMessageObserver):
     def __init__(self, node):
         messaging.BufferedMessageObserver.__init__(self)
         node.join("gui")
         node.addObserver(self)
         self.node = node
-        self.motor_demand_log_lock = threading.Lock()
-        self.motor_demand_log = []
+        self.motor_demand_log_lock = dict([(x,threading.Lock()) for x in motor_ids])
+        self.motor_demand_log = dict([(x,[]) for x in motor_ids])
     def onMotorStateMessage(self, m):
-        self.motor_demand_log_lock.acquire()
-        self.motor_demand_log.append([time.time(),abs(m.demand.prop)+abs(m.demand.hstern)+abs(m.demand.vstern)+abs(m.demand.hbow)+abs(m.demand.vbow)])
-        self.motor_demand_log_lock.release()
-#
+        print 'received motor message'
+        self.motor_demand_log_lock[m.MotorID].acquire()
+        self.motor_demand_log[m.MotorId].append([time.time(),m.speed])
+        self.motor_demand_log_lock[m.MotorID].release()
+
+#actual logger that calculates power use, saves to file and sends messages
 class slowLogger():
     def __init__(self, filename, frequency, logger, node):
-        #open log, or create new
+        #set initial values
         self.frequency = frequency
-        self.last_log_time = time.time()
         self.logger = logger
+        self.last_log_time = time.time()
         self.total_usage = 0
-        self.log_file = open(filename,'r+')
         self.node = node
+        #open log file
+        self.log_file = open(filename,'r+')
     def load_from_file(self):
+        #set the total usage to the last total usage recorded in the log
         for line in self.log_file.readlines():
             self.total_usage = float(line.split('-')[2])
     def run(self):
         while True:
-            self.logger.motor_demand_log_lock.acquire()
+            estimated_power_use = 0
+            motor_log = {}
+            for motor in motor_ids:
+                #get the last messages for each motor
+                self.logger.motor_demand_log_lock[motor].acquire()
+                current_time = time.time() #set the time of getting the log
+                motor_log[motor] = self.logger.motor_demand_log[motor]
+                if len(self.logger.motor_demand_log[motor]):
+                    self.logger.motor_demand_log[motor] = [self.logger.motor_demand_log[motor][-1]] #reset the log, leaving the last value to start the next one
+                self.logger.motor_demand_log_lock[motor].release()
+            #calculate the power since the last loop
+            estimated_power_use += estimateMotorPowerUse(motor_log)
             current_time = time.time()
-            motor_log = [x[1] for x in self.logger.motor_demand_log]
-            self.logger.motor_demand_log = []
-            self.logger.motor_demand_log_lock.release()
             interval = current_time-self.last_log_time
-            estimated_power_use = estimatePowerUse(motor_log, interval)
+            estimated_power_use += estimateConstantPowerUse(interval)
+            self.last_log_time = current_time
             self.total_usage += estimated_power_use
-            self.log_file.write(str(current_time)+'-'+str(estimated_power_use)+'-'+str(self.total_usage)+'\n')
-            self.log_file.flush()
-            print "Total power use: "+str(self.total_usage)+" Watt Hours, estimated current use: "+str(estimated_power_use/interval*3600)+" Watts"
             fraction_remaining = 1-self.total_usage/battery_total
-            print "Estimated battery remaining: "+str(fraction_remaining*100)+'%'
+            self.log_file.write(str(current_time)+'-'+str(estimated_power_use)+'-'+str(self.total_usage)+'\n')
+            self.log_file.flush() #force write to file
+            print "Total power use: "+str(self.total_usage)+" Watt Hours, estimated current use: "+str(estimated_power_use/interval*3600)+" Watts, estimated battery remaining: "+str(fraction_remaining*100)+'%'
             self.node.send(messaging.BatteryUseMessage(estimated_power_use, self.total_usage, fraction_remaining), "telemetry")
             self.last_log_time = current_time
-            print "sleeping"
+            #sleep
             time.sleep(self.frequency)
         
-#
+#reset log
 def reset():
     log = open(log_file, 'w')
     log.truncate()
     log.close
 
 def run():
+    #set up nodes
     monitor_node = cauv.node.Node('pybatmon')
     broadcast_node = cauv.node.Node('pybatbrd')
+    #set up loggers
     state_logger = motorStateLogger(monitor_node)
     slow_logger = slowLogger(log_file, 2, state_logger, broadcast_node)
     slow_logger.load_from_file()
     slow_logger.run()
     
+if __name__ == '__main__':
+    p = optparse.OptionParser()
+    p.add_option('-r', '--reset', dest='reset', default=False,
+                 action='store_true', help="create or reset battery log")
+    
+    opts, args = p.parse_args()
+    
+    if opts.reset:
+        reset()
+    else:
+        run()
