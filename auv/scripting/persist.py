3f18264a133fd4accee6a25459f35edb3c239aa7
James Crosby <jc593@cam.ac.uk>
2011-03-25 01:20:29 +0000

    working (!!!) settings persistence: python magic

--- auv/scripting/persist.py
+++ auv/scripting/persist.py
@@ -3,6 +3,10 @@ import cauv.node as node
 
 from cauv.debug import debug, info, warning, error
 
+import contextlib
+import shelve
+import traceback
+import optparse
 
 Default_Groups_To_Join = (
     'control',
@@ -13,28 +17,42 @@ Default_Messages_To_Watch = (
     'BearingAutopilotParams',
     'DepthAutopilotParams',
     'DepthCalibration',
-    'PitchAutoPilotParams',
+    'PitchAutopilotParams',
     'SetMotorMap',
     'SonarControl'
 )
 
+Ignore_Message_Attrs = (
+    'group'
+)
 
-class PersistentSetting:
-    def __init__(self, setMethodName, value):
-        self.recvMethodName = recvMethodName
-        self.sendMethodName = sendMethodName
-        self.value = value
+def dictFromMessage(message):
+    attrs = message.__class__.__dict__
+    r = {}
+    for k in attrs:
+        if not k.startswith('__') and not k in Ignore_Message_Attrs:
+            # hack hack hack hack hack
+            # I'm sure there is a simpler way of doing this, but m.__dict__ is
+            # unhelpfully empty...
+            r[k] = attrs[k].__get__(message)
+    return r
+
+def dictToMessage(message_name, attr_dict):
+    return getattr(msg, message_name+'Message')(**attr_dict)
 
 class OnAnyMessage:
-    def __init__(self, msg_name):
+    def __init__(self, msg_name, shelf):
         self.message_name = msg_name
+        self.shelf = shelf
     def onMessage(self, m):
-        debug('onMessage expect %s have %s' % (self.message_name, m))
+        debug('onMessage expect %sMessage have %s' % (self.message_name, m))
+        self.shelf[self.message_name] = dictFromMessage(m)
 
-class PersistObserver(messaging.MessageObserver):
-    def __init__(self, node, groups, watch_list):
-        messaging.MessageObserver.__init__(self)
+class PersistObserver(msg.MessageObserver):
+    def __init__(self, node, shelf, groups, watch_list):
+        msg.MessageObserver.__init__(self)
         self.__node = node
+        self.__shelf = shelf
         for group in groups:
             node.join(group)
         for message in watch_list:
@@ -42,16 +60,53 @@ class PersistObserver(messaging.MessageObserver):
         node.addObserver(self)
     
     def attachOnMessageFunc(self, name):
-        func = OnAnyMessage(name).onMessage
-        setattr(self, 'on%sMessage%s' % name, func)
+        func = OnAnyMessage(name, self.__shelf).onMessage
+        setattr(self, 'on%sMessage' % name, func)
 
+def sendSavedMessages(node, shelf):
+    info('restoring saved settings...')
+    for msg_name in shelf:
+        try:
+            attrs = shelf[msg_name]
+            debug('restoring saved %s: %s' % (msg_name, attrs))
+            m = dictToMessage(msg_name, attrs)
+            node.send(m)
+        except Exception, e:
+            warning('Exception: %s\nattempting to continue...' %
+                    traceback.format_exc())
+    info('restore complete')
+
+def persistMainLoop(cauv_node, shelf):
+    po = PersistObserver(cauv_node, shelf, Default_Groups_To_Join,
+                         Default_Messages_To_Watch)
+    help_str = 'available commands: "set" - broadcast saved '+\
+               'parameters, "exit" - stop monitoring and exit'
+    try:
+        print help_str
+        while True:
+            s = raw_input('\n> ')
+            s = s.lower().strip()
+            if s == 'exit':
+                break
+            if s == 'set':
+                sendSavedMessages(cauv_node, shelf)
+            else:
+                print help_str
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        shelf.close()
+        info('closed the shelf')
+    info('exiting...')
 
 if __name__ == '__main__':
     p = optparse.OptionParser()
-    p.add_option('-f', '--persistence-file', dest='fname', default='persistent-settings.pickle',
-                 action='store', help='file name to save/load from')
+    p.add_option('-f', '--persistence-file', dest='fname',
+                 default='/home/cauv/settings-persistence.shelf',
+                 action='store', help='file name to save/load from (python shelf)')
     p.add_option('-r', '--restore', dest='restore', default=False,
-                 action='store_true', help='do broadcast messages for saved settings')
+                 action='store_true', help='immediately broadcast messages for saved settings')
 
     opts, args = p.parse_args()
 
@@ -60,5 +115,9 @@ if __name__ == '__main__':
         exit(1)
 
     cauv_node = node.Node("persist")
-
+    shelf = shelve.open(opts.fname)
+    
+    if opts.restore:
+        sendSavedMessages(cauv_node, shelf)
+    persistMainLoop(cauv_node, shelf)
 
