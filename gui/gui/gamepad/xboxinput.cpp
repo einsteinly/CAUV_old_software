fb7db74da3b6b0da90dda1568410d56760984ae6
Andy Pritchard <andypritchard@cantab.net>
2011-03-26 21:19:01 +0000

    Many gamepad improvements

--- gui/gui/gamepad/xboxinput.cpp
+++ gui/gui/gamepad/xboxinput.cpp
@@ -3,22 +3,25 @@
 #include "xboxinput.h"
 
 #include <iostream>
+#include <stdexcept>
 
 using namespace OIS;
 using namespace cauv;
 
-XBoxInput::XBoxInput(const unsigned int id) : GamepadInput(id)
+XBoxInput::XBoxInput(const std::string vendor) : GamepadInput(vendor)
 {
 }
 
-bool XBoxInput::buttonPressed( const JoyStickEvent&, int button ) {
-    emitButton((XBox::Buttons) button, true);
-    return true;
+bool XBoxInput::buttonPressed( const JoyStickEvent& event, int button ) {
+    if(emitButton((XBox::Buttons) button, true))
+        return true;
+    else return GamepadInput::buttonPressed(event, button);
 }
 
-bool XBoxInput::buttonReleased( const JoyStickEvent&, int button ) {
-    emitButton((XBox::Buttons) button, false);
-    return true;
+bool XBoxInput::buttonReleased( const JoyStickEvent& event, int button ) {
+    if(emitButton((XBox::Buttons) button, false))
+        return true;
+    else return GamepadInput::buttonReleased(event, button);
 }
 
 bool XBoxInput::axisMoved( const JoyStickEvent &arg, int axis )
@@ -29,61 +32,32 @@ bool XBoxInput::axisMoved( const JoyStickEvent &arg, int axis )
     }
 
     switch (axis) {
-        case XBox::Joy_L_X:
-            Q_EMIT Joy_L_X(value);
-            break;
-        case XBox::Joy_L_Y:
-            Q_EMIT Joy_L_Y(value);
-            break;
-        case XBox::Joy_R_X:
-            Q_EMIT Joy_R_X(value);
-            break;
-        case XBox::Joy_R_Y:
-            Q_EMIT Joy_R_Y(value);
-            break;
-        default:
-            throw "Axis does not exist";
+    case XBox::Joy_L_X:
+        Q_EMIT Joy_L_X(value);
+        break;
+    case XBox::Joy_L_Y:
+        Q_EMIT Joy_L_Y(value);
+        break;
+    case XBox::Joy_R_X:
+        Q_EMIT Joy_R_X(value);
+        break;
+    case XBox::Joy_R_Y:
+        Q_EMIT Joy_R_Y(value);
+        break;
+    case XBox::Trigger_L:
+        Q_EMIT Trigger_L(value);
+        break;
+    case XBox::Trigger_R:
+        Q_EMIT Trigger_R(value);
+        break;
+    default:
+        GamepadInput::axisMoved(arg, axis);
     }
 
     return true;
 }
 
-bool XBoxInput::povMoved( const JoyStickEvent &arg, int pov )
-{
-        if( arg.state.mPOV[pov].direction & Pov::North ) //Going up
-            Q_EMIT Up();
-        else if( arg.state.mPOV[pov].direction & Pov::South ) //Going down
-            Q_EMIT Down();
-
-        if( arg.state.mPOV[pov].direction & Pov::East ) //Going right
-            Q_EMIT Right();
-        else if( arg.state.mPOV[pov].direction & Pov::West ) //Going left
-            Q_EMIT Left();
-
-        if( arg.state.mPOV[pov].direction == Pov::Centered ) //stopped/centered out
-            Q_EMIT Centered();
-
-        return true;
-}
-
-bool XBoxInput::vector3Moved( const JoyStickEvent &, int )
-{
-        //not used for XBox
-        return true;
-}
-
-
-void XBoxInput::printIt(bool it) const {
-    std::cout << "Button value: " << it << std::endl;
-}
-
-void XBoxInput::printIt(int it) const {
-    std::cout << "Axis value: " << it << std::endl;
-}
-
-void XBoxInput::emitButton( XBox::Buttons button, bool state ) {
-
-    std::cout << "x box button: " << button << std::endl;
+bool XBoxInput::emitButton( XBox::Buttons button, bool state ) {
 
     switch(button){
     case XBox::A:
@@ -98,33 +72,43 @@ void XBoxInput::emitButton( XBox::Buttons button, bool state ) {
     case XBox::Y:
         Q_EMIT Y(state);
         break;
+    case XBox::XBox:
+        Q_EMIT XBox(state);
+        break;
     case XBox::Back:
         Q_EMIT Back(state);
         break;
     case XBox::Start:
         Q_EMIT Start(state);
         break;
-    case XBox::LT:
-        Q_EMIT LT(state);
-        break;
     case XBox::LB:
         Q_EMIT LB(state);
-        break;
-    case XBox::RT:
-        Q_EMIT RT(state);
         break;
     case XBox::RB:
         Q_EMIT RB(state);
         break;
-    case XBox::JoyLClick:
-        Q_EMIT JoyLClick(state);
+    case XBox::Up:
+        Q_EMIT Up(state);
         break;
-    case XBox::JoyRClick:
-        Q_EMIT JoyRClick(state);
+    case XBox::Down:
+        Q_EMIT Down(state);
+        break;
+    case XBox::Left:
+        Q_EMIT Left(state);
+        break;
+    case XBox::Right:
+        Q_EMIT Right(state);
+        break;
+    case XBox::Joy_L_Click:
+        Q_EMIT Joy_L_Click(state);
+        break;
+    case XBox::Joy_R_Click:
+        Q_EMIT Joy_R_Click(state);
         break;
     default:
-        throw "Unknown button";
+        return false;
     }
+    return true;
 }
 
 
