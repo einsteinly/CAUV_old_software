2f14480d4acd81bbb28037746bbdb49b7ce3b482
James Crosby <jc593@cam.ac.uk>
2011-06-29 14:47:36 +0100

    missing import

--- auv/scripting/utils/hacks.py
+++ auv/scripting/utils/hacks.py
@@ -1,9 +1,11 @@
+from sys import float_info
+from math import frexp, ldexp
 
 def incFloat(f):
     # increment a floating point number by the smallest possible amount
     # not sure this works with negative numbers
     if f == 0.0:
-        return sys.float_info.min
-    m, e = math.frexp(f)
-    return math.ldexp(m + sys.float_info.epsilon / 2, e)
+        return float_info.min
+    m, e = frexp(f)
+    return ldexp(m + float_info.epsilon / 2, e)
 
