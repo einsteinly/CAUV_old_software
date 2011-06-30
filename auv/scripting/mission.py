3d35347d702fd3fa55e52d53f21186ae948e6792
Andy Pritchard <andypritchard@cantab.net>
2011-06-30 14:40:27 +0100

    pipeline node mutex issues fix

--- auv/scripting/mission.py
+++ auv/scripting/mission.py
@@ -3,7 +3,7 @@ from AI_classes import detectorCondition, aiTask
 #a dict of tasks that can be added or removed as neccessary
 task_list = [
             aiTask('test', 'test', 1, detectors_enabled=False, conditions = [detectorCondition('test_detector', 'test'),]),
-            aiTask('pipe', 'pipe', 1, conditions = [detectorCondition('pipe_detector', 'pipe'),]),
+            aiTask('pipe_follow', 'pipe_follow', 1, conditions = [detectorCondition('pipe_detector', 'pipe_detector'),]),
             aiTask('circle_buoy', 'circle_buoy', 1, conditions = [detectorCondition('buoy_detector', 'buoy_detector'),]),
             aiTask('avoid_collision', 'avoid_collision', 1, conditions = [detectorCondition('collision_detector', 'collision_detector'),]),
             #aiTask(task_name, script, priority, running_priority=priority, detectors_enabled=False, conditions=None)
@@ -11,6 +11,7 @@ task_list = [
 
 #a list of tasks to look out for at the beggining
 initial_tasks = [
+                'pipe_follow',
                 #'circle_buoy'
                 'test',
                 #'avoid_collision'
