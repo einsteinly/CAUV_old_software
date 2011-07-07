2f808915237f5d98856906c59c8db0e78255aa4a
James Crosby <jc593@cam.ac.uk>
2011-07-07 23:58:01 +0100

    changes from qualifying (dfu, etc...); pressure sensor hack not included (fuse was blown)

--- auv/scripting/script_library/start.py
+++ auv/scripting/script_library/start.py
@@ -4,9 +4,9 @@ from cauv.debug import debug, warning, error, info
 import time
 
 class scriptOptions(aiScriptOptions):
-    depth = 1.0
+    depth = 1.2
     forward_time = 30
-    forward_speed = 64
+    forward_speed = 120
     already_run = False
     class Meta:
         dynamic = ['already_run']
@@ -18,10 +18,14 @@ class script(aiScript):
             return
         self.ai.task_manager.modify_task_options(self.task_name, {'already_run':True})
         self.log('Diving to %d to start mission' %(self.options.depth))
-        self.auv.depthAndWait(self.options.depth)
+        self.auv.depthAndWait(self.options.depth, timeout=5)
         self.log('Heading forwards through validation gate')
-        self.auv.prop(forward_speed)
-        time.sleep(self.options.forward_time)
+        self.auv.prop(self.options.forward_speed)
+        st = time.time()
+        self.auv.depth(self.options.depth)
+        while time.time()  - st < self.options.forward_time:
+            time.sleep(0.5)
+            self.auv.prop(self.options.forward_speed)
         self.auv.prop(0)
         self.notify_exit('SUCCESS')
         
--- auv/scripting/unused/dfu.py
+++ auv/scripting/unused/dfu.py
@@ -13,35 +13,17 @@ import traceback
 def dfu(): 
     node = cauv.node.Node('py-dfu')
     auv = control.AUV(node)
-    
-    print 'setting calibration...'
-    # set-up calibration factors
-    node.send(msg.DepthCalibrationMessage(
-        -912.2/96.2, 1.0/96.2, -912.2/96.2, 1.0/96.2
-    ), "control")
-
-    auv.bearingParams(1, 0, -80, 1)
-    auv.depthParams(40, 0, 0, 1)
-    #auv.pitchParams(1, 0, 0, 1)
-
-    auv.propMap(10, -10, 127, -127)
-    auv.vbowMap(10, -10, 127, -127)
-    auv.hbowMap(10, -10, 127, -127)
-    auv.vsternMap(10, -10, 127, -127)
-    auv.hsternMap(10, -10, 127, -127)
 
     time.sleep(2)
-    #bearing = auv.getBearing()
+    bearing = auv.getBearing()
     #if bearing is None:
     #    print 'no bearing information!'
     #    time.sleep(5)
     #    bearing = 90
-    bearing = 325
-
+    auv.bearing(bearing)
     try:
         print 'setting bearing:'
-        auv.bearing(bearing)
-        time.sleep(2)
+        time.sleep(4)
 
         print 'diving...'
         auv.depth(2)
@@ -53,20 +35,15 @@ def dfu():
         auv.prop(0)
         time.sleep(4)
         print 'turning...'
-        auv.bearing((bearing + 180) % 360)
-        time.sleep(10)
-        print 'forwards...'
-        auv.prop(127)
-        time.sleep(40)
-        print 'surface...'
-        auv.depth(0)
-        auv.prop(0)
-        time.sleep(20)
     except Exception:
         traceback.print_exc()
         auv.depth(0)
+        auv.stop() 
+    finally:
+        auv.depth(0)
         auv.stop()
     print 'Complete'
+    auv.depth(0)
 
 if __name__ == '__main__':
     dfu()
