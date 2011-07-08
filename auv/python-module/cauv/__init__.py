baa27191ccfb4aa614625e5aa60006da87e45775
Daniel Kalotay <d.kalotay@gmail.com>
2011-07-08 16:01:25 +0200

    add python module paths for debug and release and small fixes to displacement.

--- auv/python-module/cauv/__init__.py
+++ auv/python-module/cauv/__init__.py
@@ -1,2 +1,14 @@
 # CAUV python module: messaging and higher-level control
+import os, sys
+
+try:
+    cauv_python_mode = os.environ['CAUV_PYTHON_MODE']
+    extra_dir = cauv_python_mode.lower()
+except KeyError:
+    extra_dir = 'release'
+finally:
+    extra_paths = []
+    for path in sys.path:
+        extra_paths.append(os.path.join(path, extra_dir))
+    sys.path.extend(extra_paths)
 
