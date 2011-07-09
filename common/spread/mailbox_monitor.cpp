170e4990be07f914122af579dc9df00d7f48b26f
James Crosby <jc593@cam.ac.uk>
2011-07-09 10:01:32 +0200

    add count down on thread interrupt attempts

--- common/spread/mailbox_monitor.cpp
+++ common/spread/mailbox_monitor.cpp
@@ -46,11 +46,12 @@ void MailboxEventMonitor::stopMonitoring() {
     // async
     if (m_thread.get_id() != boost::thread::id())
     {
+        int c = 0;
         do{
             debug() << "Interrupting monitor thread";
             m_thread.interrupt();
             m_thread.timed_join(boost::posix_time::seconds(1));
-        }while(m_thread.joinable());
+        }while(m_thread.joinable() && c++ < 30);
     } else {
         // sync
         debug() << "No internal monitor thread to interrupt"
