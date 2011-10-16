b21492cefc01a565035a740355d7e52ac9c8e6a0
James Crosby <jc593@cam.ac.uk>
2011-10-16 10:46:26 +0100

    copyright attribution, first version of license file, start attributions file for external projects.

--- messages-python/cppmess-xmessage.template.h
+++ messages-python/cppmess-xmessage.template.h
@@ -1,3 +1,17 @@
+/* Copyright 2011 Cambridge Hydronautics Ltd.
+ *
+ * Cambridge Hydronautics Ltd. licenses this software to the CAUV student
+ * society for all purposes other than publication of this source code.
+ * 
+ * See license.txt for details.
+ * 
+ * Please direct queries to the officers of Cambridge Hydronautics:
+ *     James Crosby    james@camhydro.co.uk
+ *     Andy Pritchard   andy@camhydro.co.uk
+ *     Leszek Swirski leszek@camhydro.co.uk
+ *     Hugo Vincent     hugo@camhydro.co.uk
+ */
+
 /***  This is a generated file, do not edit ***/
 \#ifndef __CAUV_${m.name.upper()}_MESSAGE_H__
 \#define __CAUV_${m.name.upper()}_MESSAGE_H__
