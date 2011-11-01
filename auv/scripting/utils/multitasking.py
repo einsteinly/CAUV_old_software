ea2a86050838f3e7b494ccdd53a75c9acc9070fb
Douglas Young <dy245@cam.ac.uk>
2011-11-01 23:10:11 +0000

    fix watch.py spewing out messages after supposedly exiting normally
    This was caused by the python finally: statement in a forked process
    trying to stop a non-existant thread

--- auv/scripting/utils/multitasking.py
+++ auv/scripting/utils/multitasking.py
@@ -13,23 +13,29 @@ def spawnDaemon(func):
             return
     except OSError, e:
         print >>sys.stderr, "fork #1 failed: %d (%s)" % (e.errno, e.strerror)
-        sys.exit(1)
+        os._exit(os.EX_OSERR)
 
     # decouple from parent environment
-    # ... don't
+    # ... don't (bad daemon ettiquite, but it's necessary for now)
     #os.chdir("/")
     os.setsid()
     #os.umask(0)
 
     # do second fork
+    # os._exit() is less 'nice' because it doesn't allow python to fully unwind
+    # like a normal exit (executing finallys and such), but in this case we don't
+    # want it to because the forked environment is probably bogus since threads
+    # aren't carried across the fork and other such problems. This also means
+    # that func() really should only be doing very simple stuff like spawning a
+    # new process or otherwise used with care.
     try:
         pid = os.fork()
         if pid > 0:
             # exit from second parent
-            sys.exit(0)
+            os._exit(os.EX_OK)
     except OSError, e:
         print >>sys.stderr, "fork #2 failed: %d (%s)" % (e.errno, e.strerror)
-        sys.exit(1)
+        os._exit(os.EX_OSERR)
     # do stuff
     func()
 
