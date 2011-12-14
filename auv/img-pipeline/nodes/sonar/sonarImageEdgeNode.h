230990ac2512a20bb3f0f8569d1420b03c2b3131
Jason Wyatt <devnull@localhost>
2011-12-14 13:44:38 +0000

    Modified SonarImageEdgeNode to be more helpful added example river edge finding (seems to work on bothe sharp and shallow banks)

--- auv/img-pipeline/nodes/sonar/sonarImageEdgeNode.h
+++ auv/img-pipeline/nodes/sonar/sonarImageEdgeNode.h
@@ -31,13 +31,15 @@ class SonarImageEdgeNode : public Node {
             // parameters: scale factor, interpolation mode
             registerParamID<int>("min amplitude", 20, "lowest amplitude definately in image");
             registerParamID<int>("min width", 2, "length lowest amplitude needs to be to be counted");
+            registerParamID<int>("line width", 2, "length to draw white");
         }
         
     protected:
         struct applyFilter: boost::static_visitor<void>{
-            applyFilter(int min_amp, int min_width)
+            applyFilter(int min_amp, int min_width, int line_width)
                 : m_min_amp(min_amp),
-                  m_min_width(min_width){
+                  m_min_width(min_width),
+                  m_line_width(line_width){
             }
             void operator()(cv::Mat) const{
                 throw parameter_error("only polar images are supported");
@@ -46,12 +48,12 @@ class SonarImageEdgeNode : public Node {
                 const int rows = a.mat.rows;
                 const int cols = a.mat.cols;
                 
-                // apply to each row individually
-                for(int row=0; row<rows; row++){
-                    int successive = 0; // number of points in a row greater than amplitude
-                    int pos = 0; //next point to set value at
+                // apply to each col individually
+                for(int col=0; col<cols; col++){
+                    int successive = 0; // number of points greater than amplitude
+                    int pos = rows-1; //next point to set value at
                     // set all black until we reach success
-                    for(int col=cols-1; col>=0; col--){
+                    for(int row=rows-1; row>=0; row--){
                         if(a.mat.at<uint8_t>(row, col)>=m_min_amp){
                             successive++;
                             if(successive>=m_min_width){
@@ -60,17 +62,24 @@ class SonarImageEdgeNode : public Node {
                         }
                         else{
                             //set all previous to black as well
-                            while(pos<=col){
-                                a.mat.at<uint8_t>(row, pos) = 0;
-                                pos++;
+                            while(pos>=row){
+                                a.mat.at<uint8_t>(pos, col) = 0;
+                                pos--;
                             }
                             successive = 0;
                         }
                     }
-                    //set remaining white
-                    while(pos<=cols){
-                        a.mat.at<uint8_t>(row, pos) = 255;
-                        pos++;
+                    //set line width white
+                    int w = m_line_width;
+                    while(pos>=0 and w>0){
+                        a.mat.at<uint8_t>(pos, col) = 255;
+                        pos--;
+                        w--;
+                    }
+                    //and remaining black
+                    while(pos>=0){
+                        a.mat.at<uint8_t>(pos, col) = 0;
+                        pos--;
                     }
                 }
             }
@@ -79,6 +88,7 @@ class SonarImageEdgeNode : public Node {
             }
             const int m_min_amp;
             const int m_min_width;
+            const int m_line_width;
         };
         out_map_t doWork(in_image_map_t& inputs){
             out_map_t r;
@@ -87,11 +97,12 @@ class SonarImageEdgeNode : public Node {
             
             const int min_amp = param<int>("min amplitude");
             const int min_width = param<int>("min width");
+            const int line_width = param<int>("line width");
             
             augmented_mat_t in = img->augmentedMat();
 
             try{
-                boost::apply_visitor(applyFilter(min_amp, min_width), in);
+                boost::apply_visitor(applyFilter(min_amp, min_width, line_width), in);
                 r["polar image"] = img;
             }catch(cv::Exception& e){
                 error() << "SonarImageEdgeNode:\n\t"
