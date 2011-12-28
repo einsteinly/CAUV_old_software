b135f33d2d0fb3d80d4abedf81449890f24a1d71
Douglas Young <dy245@cam.ac.uk>
2011-12-28 01:33:21 +0000

    add include-what-you-use wrapper shell script

---
+++ cmake/include-what-you-use-wrapper.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# cmake expects whatever compiler it's given to work, 
+# and since iwyu just checks what it needs to and then exits
+# with an error code, cmake will barf. To fix this, this script 
+# acts like clang but also calls iwyu. it could call gcc but that
+# causes problems because cmake feeds slightly different flags to
+# clang and gcc so the build will fail about halfway through.
+#
+# to use:
+# CC=clang CXX=software.hg/cmake/include-what-you-use-wrapper.sh cmake [args]
+# IWYU_OUTPUT_DIR=`pwd` make
+
+# the version of clang built against can't find some directories, so they're added here
+iwyu_flags="-I /usr/include/include-what-you-use $@"
+
+#enable logging info to one file
+if [ -z "$IWYU_OUTPUT_DIR" ]
+then
+    include-what-you-use $iwyu_flags
+else
+    echo logging to $IWYU_OUTPUT_DIR/include-what-you-use.log
+    include-what-you-use $iwyu_flags &>> "$IWYU_OUTPUT_DIR/include-what-you-use.log"
+fi
+clang++ $@
+exit $?
