48c34c0ec8be7301261f5f1f88b72802e7ebf914
Andy Pritchard <andy.pritchard@camhydro.co.uk>
2012-02-03 21:13:50 +0000

    file name case change

---
+++ gui/gui/plugins/ai/ainode.cpp
@@ -0,0 +1,34 @@
+/* Copyright 2011 Cambridge Hydronautics Ltd.
+ *
+ * Cambridge Hydronautics Ltd. licenses this software to the CAUV student
+ * society for all purposes other than publication of this source code.
+ *
+ * See license.txt for details.
+ *
+ * Please direct queries to the officers of Cambridge Hydronautics:
+ *     James Crosby    james@camhydro.co.uk
+ *     Andy Pritchard   andy@camhydro.co.uk
+ *     Leszek Swirski leszek@camhydro.co.uk
+ *     Hugo Vincent     hugo@camhydro.co.uk
+ */
+
+#include "ainode.h"
+
+#include <debug/cauv_debug.h>
+
+#include <gui/core/framework/elements/style.h>
+
+#include <generated/types/GuiaiGroup.h>
+
+using namespace cauv;
+using namespace cauv::gui;
+
+
+AiNode::AiNode(QGraphicsItem *parent) :
+        liquid::LiquidNode(AI_Node_Style, parent)
+{
+}
+
+AiNode::~AiNode(){
+    debug(2) << "~AINode()";
+}
