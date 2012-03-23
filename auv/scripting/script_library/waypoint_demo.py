119a05c4bc2740334508c951598f560e125a2ae1
Jason Wyatt <devnull@localhost>
2012-03-23 00:11:28 +0000

    slight fixes. if this goes in the wrong direction, it's because of messed up coordinates in sim.py

--- auv/scripting/script_library/waypoint_demo.py
+++ auv/scripting/script_library/waypoint_demo.py
@@ -2,7 +2,10 @@ from AI_classes import aiScript, aiScriptOptions
 
 from cauv.debug import info, warning, debug, error
 
+from utils.vectormath import vec
+
 from math import atan, degrees
+import time
 
 class scriptOptions(aiScriptOptions):
     error = 0.5
@@ -14,6 +17,7 @@ class script(aiScript):
         aiScript.__init__(self, *args, **kwargs)
         self.waypoints = []
     def run(self):
+        #self.waypoints = (vec(0,0), vec(4.5,4.5), vec(-4.5,-6))
         while True:
             ans = raw_input('Mark waypoint (y/n)')
             if ans == 'y':
@@ -23,14 +27,20 @@ class script(aiScript):
             else:
                 debug("Didn't enter valid answer")
         print self.waypoints
+        if self.auv.position == None:
+            #no position data, wait for broadcast
+            time.sleep(1.0)
+            if self.auv.position == None:
+                #if still none, then give up (since clearly not getting location
+                return 'FAILURE'
         for waypoint in self.waypoints:
             vector_to = waypoint-self.auv.position
             #             |N/y  /
             #             |    /
-            #             |   /
-            #             |  /
+            #             |___/
+            #             |b /
             #             | /
-            #_____________|/______________x
+            #_____________|/_____________x
             #             |
             #             |
             #             |
@@ -43,9 +53,12 @@ class script(aiScript):
                 if vector_to.y<0:
                     bearing+180
                 bearing = bearing%360
+                debug('Heading on a %f degree bearing, direction vector '+str(vector_to))
                 self.auv.bearing(bearing)
                 #go forward
                 self.auv.prop(50)
+                time.sleep(1.0)
+                vector_to = waypoint-self.auv.position
             self.auv.prop(0)
             debug("Arrived at waypoint")
         return "SUCCESS"
