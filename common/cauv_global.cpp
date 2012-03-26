9d8e784840c9d7a634c7fa7de8232c74036fec23
James Crosby <jc593@cam.ac.uk>
2012-03-26 15:04:25 +0100

    time well spent

--- common/cauv_global.cpp
+++ common/cauv_global.cpp
@@ -20,12 +20,11 @@
 
 #include "cauv_global.h"
 
-using namespace std;
 using namespace cauv;
 
 cauv_global* cauv_global::m_current = 0;
 
-cauv_global::cauv_global() //: m_socket(0)
+cauv_global::cauv_global()
 {
 }
 cauv_global& cauv_global::current()
@@ -35,100 +34,74 @@ cauv_global& cauv_global::current()
 	return *m_current;
 }
 
-/*
-void cauv_global::set_socket(CommunicatingSocket* socket)
-{
-	current().m_socket = socket;
-}
-*/
-
-void cauv_global::send_trace(const string&/* msg*/) const
-{
-/*	TraceMessage m;
- *  m.msg(msg);
-	try
-    {
-        if (m_mailbox)
-            m_mailbox->sendMessage(m, LOW_MESS);
-    }
-    catch (SpreadException& e)
-    {
-        cout << "Error sending debug data: " << e.what() << endl;
-    }
-*/
-}
-void cauv_global::send_error(const string&/* msg*/) const
-{
-/*	ErrorMessage m(msg);
-	try
-    {
-        if (m_socket)
-            m_socket->sendMessage(m);
-		else
-			cout << "\E[1;30mError not sent: no socket.\E[m" << endl;
-    }
-    catch (SocketException& e)
-    {
-        cout << "Error sending debug data: " << e.what() << endl;
-    }
-*/
-}
-
-void cauv_global::print_logo(char* start, char* end, const string& module_name)
-{
-	char* cur = start;
-    stringstream ss;
-	ss << endl;
-    while (cur != end)
+void cauv_global::print_logo(
+    const char* colour_start,
+    const char* colour_end,
+    const char* shape_start,
+    const char* shape_end,
+    const std::string& module_name
+){
+	const char* colour_cur = colour_start;
+	const char* shape_cur = shape_start;
+    std::stringstream ss;
+	ss << std::endl;
+    while (colour_cur != colour_end && shape_cur < shape_end)
 	{
-		if (*cur == '\n')
-			ss << BashControl::Reset << endl;
-		else if (*cur == *(cur-1))
-			ss << ' ';
+		if (*colour_cur == '\n')
+			ss << BashControl::Reset << std::endl;
+		else if (*shape_cur == *(shape_cur-1) && *colour_cur == *(colour_cur-1))
+			ss << *shape_cur;
 		else
 		{
-			switch(*cur)
+			switch(*colour_cur)
 			{
 				case 'K':
-					ss << BashBackground::Black << ' ';
+					ss << BashBackground::Black;
                     break;
 				case 'R':
-					ss << BashBackground::Red << ' ';
+					ss << BashBackground::Red;
 					break;
 				case 'G':
-					ss << BashBackground::Green << ' ';
+					ss << BashBackground::Green;
 					break;
 				case 'Y':
-					ss << BashBackground::Yellow << ' ';
+					ss << BashBackground::Yellow;
 					break;
 				case 'B':
-					ss << BashBackground::Blue << ' ';
+					ss << BashBackground::Blue;
+                    break;
+				case 'b':
+					ss << BashControl::Reset << BashColour::Blue;
 					break;
 				case 'M':
-					ss << BashBackground::Magenta << ' ';
+					ss << BashBackground::Magenta;
 					break;
 				case 'C':
-					ss << BashBackground::Cyan << ' ';
+					ss << BashBackground::Cyan;
 					break;
 				case 'W':
-					ss << BashBackground::White << ' ';
+					ss << BashBackground::White;
+					break;
+				case 'w':
+					ss << BashControl::Reset << BashColour::White;
 					break;
 				case ' ':
-					ss << BashControl::Reset << ' ';
+					ss << BashControl::Reset;
 					break;
 				case 'c':
 				{
 					ss << BashColour::Blue << BashIntensity::Bold;
 					int len = 0;
-					while (cur != end && *cur == 'c')
+					while (colour_cur != colour_end && *colour_cur == 'c')
                     {
 						len++;
-					    cur++;
+					    colour_cur++;
+                        shape_cur++;
                     }
                     if (len >= (int)module_name.size())
 					{
 						int start = (len - module_name.size())/2;
-						string pad(start, ' ');
+						std::string pad(start, ' ');
 						ss << pad << module_name << pad;
 					}
 					else
@@ -140,20 +113,28 @@ void cauv_global::print_logo(char* start, char* end, const string& module_name)
 				default:
 					break;
 			}
+            ss << *shape_cur;
 		}
-        cur++;
+        colour_cur++;
+        shape_cur++;
 	}
-    cout << ss.str();
+    std::cout << ss.str();
 }
 
 
-void cauv_global::print_module_header(const string& module_name)
+void cauv_global::print_module_header(const std::string& module_name)
 {
-    static char cauv_logo_large[] = {
-        #include <generated/cauv_logo_large.h>
+    static char cauv_logo_colours[] = {
+        #include <generated/cauv_logo_colours.h>
     };
-	cauv_global::print_logo(cauv_logo_large,
-                            cauv_logo_large + sizeof(cauv_logo_large),
+    static char cauv_logo_shape[] = {
+        #include <generated/cauv_logo_shape.h>
+    };
+
+	cauv_global::print_logo(cauv_logo_colours,
+                            cauv_logo_colours + sizeof(cauv_logo_colours),
+                            cauv_logo_shape,
+                            cauv_logo_shape + sizeof(cauv_logo_shape),
                             module_name);
 }
 
