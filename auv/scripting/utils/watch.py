c1a7731cb4b44a144e19b23f1d572868f2a703cd
Douglas Young <dy245@cam.ac.uk>
2012-05-18 23:24:29 +0100

    Split watch2.py into a separate class which can be used in other scripts

---
+++ auv/scripting/utils/watch.py
@@ -0,0 +1,144 @@
+import collections
+import errno
+import imp
+import os
+import pwd
+import resource
+import shlex
+import subprocess
+import sys
+import time
+import watchfuncs
+
+import utils.multitasking
+from cauv.debug import debug, info, warning, error
+
+class SessionNotFound(Exception):
+    pass
+
+def load_session(filename):
+    for suffix, mode, type in imp.get_suffixes():
+        if filename.endswith(suffix):
+            with open(filename, mode) as session_file:
+                return imp.load_module("session", session_file, "", (suffix, mode, type))
+    raise SessionNotFound()
+
+class WatchProcess:
+    def __init__(self, process, settings):
+        self.p = process
+        self.pid = None
+        self.restart = True
+        self.settings = settings
+
+    def exec_proc(self):
+        format_dict = {'SDIR': os.path.abspath(self.settings.script_dir), "BDIR": os.path.abspath(self.settings.bin_dir), "D": ''}
+        command = self.p.cmds[0].format(**format_dict)
+        work_dir = self.p.work_dir.format(**format_dict)
+        os.chdir(work_dir)
+
+        if self.p.user is None:
+            username = self.settings.user
+        else:
+            username = self.p.user
+        debug('running \'{}\' in directory {} as {}'.format(command, work_dir, username))
+        try:
+            uid, gid = pwd.getpwnam(username)[2:4]
+            if sys.platform == 'darwin':
+                # no such thing as a saved id...
+                os.setgid(gid)
+                os.setuid(uid)
+            else:
+                os.setresgid(gid,gid,gid)
+                os.setresuid(uid,uid,uid)
+        except OSError as e:
+            if e.errno == errno.EPERM:
+                warning("Can't change user. Running as current user instead")
+            else:
+                raise
+
+        null_fd = os.open("/dev/null", os.O_RDWR)
+        os.dup2(null_fd, sys.stdin.fileno())
+        os.dup2(null_fd, sys.stdout.fileno())
+        os.dup2(null_fd, sys.stderr.fileno())
+
+        argv = shlex.split(command)
+        os.execlp(argv[0], *argv)
+
+    def tick(self):
+        if self.pid is not None:
+            try:
+                os.kill(self.pid, 0)
+            except OSError as e:
+                if e.errno == errno.ESRCH:
+                    self.pid = None
+            if self.pid is None:
+                warning("Process {} died!".format(self.p.name))
+                self.restart = self.p.death_callback(self.p)
+        if self.pid is None:
+            self.pid = self.p.pid_func()
+            if self.pid is not None:
+                info("found PID {} for process {}".format(self.pid, self.p.name))
+        if self.pid is None and self.restart:
+            self.pid = utils.multitasking.spawnDaemon(self.exec_proc)
+
+    def kill(self, sig):
+        if self.pid is None:
+            self.pid = self.p.pid_func()
+        if self.pid is not None:
+            info("Killing PID {} ({})".format(self.pid, self.p.name))
+            try:
+                os.kill(self.pid, sig)
+            except OSError as e:
+                error("Could not kill PID: {}".format(e))
+        else:
+            warning("Could not find PID for {}".format(self.p.name))
+
+def setup_core_dumps():
+    core_pattern = "/tmp/corefiles/%e.%d.%t.%p"
+    try:
+        try:
+            os.mkdir("/tmp/corefiles", 0777)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                error("Could not create corefile directory")
+                raise
+        with open("/proc/sys/kernel/core_pattern", "r") as pattern_file:
+            if not pattern_file.read().startswith(core_pattern):
+                with open("/proc/sys/kernel/core_pattern", "w") as pattern_w_file:
+                    pattern_w_file.write(core_pattern)
+        soft, hard = resource.getrlimit(resource.RLIMIT_CORE)
+        resource.setrlimit(resource.RLIMIT_CORE, (min(hard, 10*1024*1024), hard))
+    except IOError as e:
+        if e.errno == errno.EACCES:
+            error("Could not change core pattern: not running as root?")
+        elif e.errno == errno.ENOENT:
+            error("Could not find core pattern file. Probably using a Mac...")
+        else:
+            raise
+    
+Settings = collections.namedtuple('Settings', ['user', 'script_dir', 'bin_dir'])
+
+class Watcher:
+    def __init__(self, processes = None, core_dumps = False, log_dir = None,
+            default_user = None, script_dir = '', bin_dir = ''):
+        if core_dumps:
+            setup_core_dumps()
+        if processes is None:
+            processes = []
+        if log_dir:
+            os.environ["CAUV_LOG_DIR"] = log_dir
+
+        self.settings = Settings(default_user, script_dir, bin_dir)
+
+        self.processes = [WatchProcess(p, self.settings) for p in processes]
+
+    def monitor(self, tick_time = 1):
+        while True:
+            for p in self.processes:
+                p.tick()
+            time.sleep(tick_time)
+
+    def kill(self, signal):
+        for p in self.processes:
+            p.kill(signal)
+
--- auv/scripting/watch2.py
+++ auv/scripting/watch2.py
@@ -3,23 +3,12 @@
 import sys
 import argparse
 import imp
-import errno
-import subprocess
 import utils.multitasking
 import os
-import shlex
-import time
-import resource
 import pwd
+import utils.watch as watch
 from cauv.debug import debug, info, warning, error
 from cauv.messaging import debugParseOptions
-
-def load_session(filename):
-    for suffix, mode, type in imp.get_suffixes():
-        if args.session.endswith(suffix):
-            with open(args.session, mode) as session_file:
-                return imp.load_module("session", session_file, "", (suffix, mode, type))
-    raise SessionNotFound()
 
 def currentUser():
     return pwd.getpwuid(os.getuid())[0]
@@ -30,11 +19,8 @@ parser.add_argument("--session", "-s", help="session file to use")
 
 args, unknown = parser.parse_known_args()
 
-class SessionNotFound(Exception):
-    pass
-
 if args.session:
-    session = load_session(args.session)
+    session = watch.load_session(args.session)
 try:
     arg_group = parser.add_argument_group(title=args.session)
     session.get_arguments(arg_group)
@@ -58,77 +44,14 @@ if args.help or not args.session:
     parser.print_help()
     sys.exit(1)
 
-class WatchProcess:
-    def __init__(self, process):
-        self.p = process
-        self.pid = None
-        self.restart = True
+if args.user is None:
+    try:
+        args.user = os.environ["SUDO_USER"]
+    except KeyError:
+        pass
 
-    def exec_proc(self):
-        format_dict = {'SDIR': os.path.abspath(args.script_dir), "BDIR": os.path.abspath(args.bin_dir), "D": ''}
-        command = self.p.cmds[0].format(**format_dict)
-        work_dir = self.p.work_dir.format(**format_dict)
-        os.chdir(work_dir)
-
-        if self.p.user is None:
-            username = args.user
-        else:
-            username = self.p.user
-        debug('running \'{}\' in directory {} as {}'.format(command, work_dir, username))
-        try:
-            uid, gid = pwd.getpwnam(username)[2:4]
-            if sys.platform == 'darwin':
-                # no such thing as a saved id...
-                os.setgid(gid)
-                os.setuid(uid)
-            else:
-                os.setresgid(gid,gid,gid)
-                os.setresuid(uid,uid,uid)
-        except OSError as e:
-            if e.errno == errno.EPERM:
-                warning("Can't change user. Running as current user instead")
-            else:
-                raise
-
-        null_fd = os.open("/dev/null", os.O_RDWR)
-        os.dup2(null_fd, sys.stdin.fileno())
-        os.dup2(null_fd, sys.stdout.fileno())
-        os.dup2(null_fd, sys.stderr.fileno())
-
-        argv = shlex.split(command)
-        os.execlp(argv[0], *argv)
-
-    def tick(self):
-        if self.pid is not None:
-            try:
-                os.kill(self.pid, 0)
-            except OSError as e:
-                if e.errno == errno.ESRCH:
-                    self.pid = None
-            if self.pid is None:
-                warning("Process {} died!".format(self.p.name))
-                self.restart = self.p.death_callback(self.p)
-        if self.pid is None:
-            self.pid = self.p.pid_func()
-            if self.pid is not None:
-                info("found PID {} for process {}".format(self.pid, self.p.name))
-        if self.pid is None and self.restart:
-            self.pid = utils.multitasking.spawnDaemon(self.exec_proc)
-
-    def kill(self, sig):
-        if self.pid is None:
-            self.pid = self.p.pid_func()
-        if self.pid is not None:
-            info("Killing PID {} ({})".format(self.pid, self.p.name))
-            try:
-                os.kill(self.pid, sig)
-            except OSError as e:
-                error("Could not kill PID: {}".format(e))
-        else:
-            warning("Could not find PID for {}".format(self.p.name))
-
-
-processes = [WatchProcess(p) for p in session.get_processes(args)]
+watcher = watch.Watcher(session.get_processes(args), args.core_dumps, args.log_dir,
+                        args.user, args.script_dir, args.bin_dir)
 
 def monitor():
     if args.daemonize:
@@ -138,46 +61,10 @@ def monitor():
         os.dup2(null_fd, sys.stdout.fileno())
         os.dup2(null_fd, sys.stderr.fileno())
 
-    while True:
-        for p in processes:
-            p.tick()
-        time.sleep(1)
-
-if args.user is None:
-    try:
-        args.user = os.environ["SUDO_USER"]
-    except KeyError:
-        pass
-
-if args.log_dir:
-    os.environ["CAUV_LOG_DIR"] = args.log_dir
-
-if args.core_dumps:
-    core_pattern = "/tmp/corefiles/%e.%d.%t.%p"
-    try:
-        try:
-            os.mkdir("/tmp/corefiles", 0777)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                error("Could not create corefile directory")
-                raise
-        with open("/proc/sys/kernel/core_pattern", "r") as pattern_file:
-            if not pattern_file.read().startswith(core_pattern):
-                with open("/proc/sys/kernel/core_pattern", "w") as pattern_w_file:
-                    pattern_w_file.write(core_pattern)
-        soft, hard = resource.getrlimit(resource.RLIMIT_CORE)
-        resource.setrlimit(resource.RLIMIT_CORE, (min(hard, 10*1024*1024), hard))
-    except IOError as e:
-        if e.errno == errno.EACCES:
-            error("Could not change core pattern: not running as root?")
-        elif e.errno == errno.ENOENT:
-            error("Could not find core pattern file. Probably using a Mac...")
-        else:
-            raise
-
+    watcher.monitor()
+    
 if args.kill is not None:
-    for p in processes:
-        p.kill(args.kill)
+    watcher.kill(args.kill)
 elif args.daemonize:
     utils.multitasking.spawnDaemon(monitor)
 else:
