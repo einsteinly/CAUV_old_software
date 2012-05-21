9d344bb1a9b062af0d63eb3cb775acc1159a9501
Douglas Young <dy245@cam.ac.uk>
2012-05-21 23:11:30 +0100

    Fix typo in last commit (cba to figure out amend in hg)

--- auv/python-module/cauv/debug.py
+++ auv/python-module/cauv/debug.py
@@ -4,5 +4,5 @@ from messaging import debug, warning, error, info, setDebugName
 _old_excepthook = sys.excepthook
 def log_excepthook(etype, evalue, tb):
     _old_excepthook(etype, evalue, tb)
-    error(''.join(traceback.format_exception(type, evalue, tb)))
+    error(''.join(traceback.format_exception(etype, evalue, tb)))
 sys.excepthook = log_excepthook
