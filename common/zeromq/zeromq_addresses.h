7f4aa382237718a73c28d34193696fc26045e472
Douglas Young <dy245@cam.ac.uk>
2012-06-09 19:32:16 +0100

    Update zeromq messaging layer for libxs 1.1.0, which is now the minimum version

--- common/zeromq/zeromq_addresses.h
+++ common/zeromq/zeromq_addresses.h
@@ -1,11 +1,19 @@
 #ifndef ZEROMQ_ADDRESSES_H
 #define ZEROMQ_ADDRESSES_H
 #include <string>
+#include <vector>
+#include <stdint.h>
 
 namespace cauv {
 
 #define NODE_MARKER_MSGID 223
 #define DAEMON_MARKER_MSGID 224
+
+typedef std::pair<bool, std::vector<uint32_t> > subscription_vec_t;
+
+std::string gen_subscription_message(subscription_vec_t);
+
+subscription_vec_t parse_subscription_message(std::string msg);
 
 std::string get_vehicle_name(void);
 
