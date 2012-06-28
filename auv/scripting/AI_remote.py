f79e7fbca097f86db277a31113794213a14ab3a9
Leszek Swirski <ls426@cam.ac.uk>
2012-06-28 14:43:33 +0100

    IPython version detection 'fix'

--- auv/scripting/AI_remote.py
+++ auv/scripting/AI_remote.py
@@ -6,7 +6,13 @@ from AI_classes import aiProcess
 
 import cPickle
 import IPython
-if [int(v) for v in IPython.__version__.split('.')] < [0,11]:
+
+# FIXME Obvious.
+try:
+    ipython_version_is_jameses_shitty_one = [int(v) for v in IPython.__version__.split('.')] < [0,11]
+except:
+    ipython_version_is_jameses_shitty_one = False
+if ipython_version_is_jameses_shitty_one:
     from IPython.Shell import IPShellEmbed as InteractiveShellEmbed #pylint: disable=E0611
 else:
     from IPython.frontend.terminal.embed import InteractiveShellEmbed #pylint: disable=E0611
