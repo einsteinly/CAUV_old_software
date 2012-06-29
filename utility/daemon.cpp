c1aea7c872ca93168eec318f73c6b8586fd170fa
Leszek Swirski <ls426@cam.ac.uk>
2012-06-29 17:37:06 +0100

    Moved vehicle_daemon out of common/zeromq, renamed it to heimdall (Norse god who chills on the end of the bifrost)

---
+++ utility/daemon.cpp
@@ -0,0 +1,27 @@
+#include "daemon.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+
+void daemonize(void) {
+    int pid = fork();
+    if(pid == 0) {
+        if(fork() > 0) {
+            exit(0);
+        }
+    } else if (pid < 0) {
+        printf("fork unsuccessful!\n");
+        exit(1);
+    } else {
+        exit(0);
+    }
+    chdir("/");
+    umask(0000);
+    freopen("/dev/null","r",stdin);
+    freopen("/dev/null","w",stdout);
+    freopen("/dev/null","w",stderr);
+}
