04e6133747bce0ca1816a495a930b36cb3cf0de3
Andy Pritchard <andy.pritchard@camhydro.co.uk>
2012-06-30 22:19:16 +0100

    indentation fix

--- gui/gui/core/framework/fluidity/fNodeInput.cpp
+++ gui/gui/core/framework/fluidity/fNodeInput.cpp
@@ -276,6 +276,7 @@ void FNodeParamInput::initView(){
     const int height_hint = 25;
 
     m_view = new NodeTreeView();
+    m_view->setIndentation(0);
     m_view->registerDelegate(nodeType<NumericNodeBase>(), boost::make_shared<NumericDelegate>(true));
     m_view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     m_view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
