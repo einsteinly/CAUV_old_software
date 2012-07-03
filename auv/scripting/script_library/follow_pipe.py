99e843cc700de849317ceeef76cc76288574d26f
Jason Wyatt <jw682@cam.ac.uk>
2012-07-03 12:13:23 +0100

    started work on updating the pipeline following

--- auv/scripting/script_library/follow_pipe.py
+++ auv/scripting/script_library/follow_pipe.py
@@ -11,7 +11,7 @@ from cauv.debug import debug, info, warning, error
 from AI_classes import aiScript, aiScriptOptions
 
 import threading
-from math import degrees, cos, sin
+from math import degrees, cos, sin, pi
 import time
 
 class scriptOptions(aiScriptOptions):
@@ -23,20 +23,16 @@ class scriptOptions(aiScriptOptions):
     ready_timeout = 30
     lost_timeout = 10
     # Calibration
-    pipe_end     = 0.4 # of image (range +0.5 to -0.5)
-    target_width = 0.1 # of image
+    pipe_end     = 0.1 # of image (range +0.5 to -0.5)
+    target_width = 0.2 # of image
     width_error  = 0.1 # of image
     centre_error = 0.2 # of image
-    align_error  = 5   # degrees 
+    align_error  = 10   # degrees 
     average_time = 1   # seconds
-    intensity_trigger = 0.10
-    y_disp_trigger = 0.3
+    angle_consistency = 0.4
     # Control
     prop_speed = 80
     strafe_kP  = -50
-    
-    #TODO: this doesn't actually do anythign at the moment
-    # its not tuned
     depth_kP   = 0.1
 
     class Meta:
@@ -47,7 +43,7 @@ class scriptOptions(aiScriptOptions):
 
 
 class script(aiScript):
-    debug_values = ['centre_error', 'angle_error', 'depth_error']
+    debug_values = ['centre_error', 'angle_error', 'depth_error', 'angle']
     def __init__(self, script_name, opts, state):
         aiScript.__init__(self, script_name, opts, state) 
         self.node.join("processing")
@@ -63,6 +59,7 @@ class script(aiScript):
         self.centre_error = 0
         self.angle_error = 0
         self.depth_error = 0
+        self.angle = 0
         
         # controllers for staying above the pipe
         self.depthControl = PIDController((self.options.depth_kP,0,0))
@@ -82,70 +79,86 @@ class script(aiScript):
             #
             # Adjust the AUV's bearing to align with the pipe
             # calculate the average angle of the lines (assumes good line finding)
-            angle = sum([x.angle for x in m.lines])/len(m.lines)
+            self.angle = sum([x.angle for x in m.lines])/len(m.lines)
             
             # calculate the bearing of the pipe (relative to the sub),
             # mod 180 as we dont want to accidentally turn the sub around
-            corrected_angle=90-degrees(angle)%180 #TODO this mod 180 probably is not needed from observation of cam following
-            debug('follow pipe: mean lines direction: %g (uncorrected=%g)' % (corrected_angle, angle))
-            
+            corrected_angle=90-degrees(self.angle)%180 #TODO this mod 180 probably is not needed from observation of cam following
+            debug('Average lines direction: %g (uncorrected=%g)' % (corrected_angle, self.angle))
             
             # work out the actual bearing of the pipe using the current bearing of the AUV
             current_bearing = self.auv.getBearing()
             if current_bearing and self.enabled:
                 self.auv.bearing((current_bearing-corrected_angle)%360) #- as angle is opposite direction to bearing
             
-            if abs(corrected_angle) < self.options.align_error: self.aligned.set()
-            else: self.aligned.clear()
+            if abs(corrected_angle) < self.options.align_error:
+                self.aligned.set()
+            else:
+                self.aligned.clear()
             
-            self.align_error = current_bearing
+            #debug info
+            self.align_error = corrected_angle
                 
             # set the flags that show if we're above the pipe
+            debug('centred:%s aligned:%s depthed:%s' %
+                    (self.centred.is_set(), self.aligned.is_set(), self.depthed.is_set())
+            )
             if self.centred.is_set() and self.aligned.is_set() and self.depthed.is_set():
                 self.ready.set()
-                debug('ready over pipeline')
                 self.log('Pipeline follower managed to align itself over the pipe.')
             else:
-                debug('centred:%s aligned:%s depthed:%s' %
-                        (self.centred.is_set(), self.aligned.is_set(), self.depthed.is_set())
-                )
                 self.ready.clear()
 
-
-    def onEllipsesMessage(self, m):
-        if m.name != self.options.centre_name:
-            debug('follow pipe: ignoring centre message %s != %s' % (m.name, self.options.centre_name))
+    def onEllipsesMessage(self, ms):
+        if ms.name != self.options.ellipses_name:
             return
+        try:
+            m = ms.ellipses[0]
+        except IndexError:
+            warning('Incorrect ellipses message received')
+        #centering
         debug('Centre error: %f' %(m.centre.x - 0.5))
-        if abs(m.x) == 0:
-            warning('centre at 0, ignoring')
+        if abs(m.centre.x) == 0:
+            warning('Centre at 0, ignoring')
             self.centred.clear()
             return
-        strafe = int(self.strafeControl.update(m.x - 0.5))
+        strafe = int(self.strafeControl.update(m.centre.x - 0.5))
         if strafe < -127:
             strafe = -127
         elif strafe > 127:
             strafe = 127
         if self.enabled:
             self.auv.strafe(strafe)
-        info('pipe follow: strafing %i' %(strafe))
+        info('Strafing %i' %(strafe))
         
+        #debug info
+        self.centre_error = m.centre.x - 0.5
                
         # y average is used to find the end of the pipe
-        yCoord = self.yAverage.update(m.y - 0.5)
-        info("pipe y coord = %f" % (yCoord,))
-        if(yCoord > self.options.pipe_end):
+        yCoord = self.yAverage.update(m.centre.y - 0.5)
+        info("Averaged centre y location coord = %f" % (yCoord,))
+        #if y is below a certain point and we are above the pipe
+        if(yCoord > self.options.pipe_end) and self.ready.is_set():
             self.pipeEnded.set()
         else: self.pipeEnded.clear()
         
-        
         # set the flag used for determining if we're above the pipe
-        debug('mx =  %g, self.options./centre_error = %g' % (float(m.x), self.options.centre_error))
-        if abs(m.x -0.5) < abs(self.options.centre_error):
+        if abs(m.centre.x -0.5) < abs(self.options.centre_error):
             self.centred.set() #ie within circle radius centre error
         else:
             self.centred.clear()
-        
+            
+        #depthing
+        if abs(self.angle%pi-m.angle%pi)>self.options.angle_consistency and \
+        abs(self.angle%pi-m.angle%pi)<pi-self.options.angle_consistency:
+            debug('Too much variation in angle between lines and ellipse (angles %f, %f respectively)' %(self.angle, m.angle))
+            return
+        depth = self.depthControl.update(self.options.target_width-m.minorRadius)
+        self.auv.depth(self.auv.current_depth+depth)
+        if abs(self.options.target_width-m.minorRadius) < self.options.width_error:
+            self.depthed.set()
+        else:
+            self.depthed.clear()
 
     def followPipeUntil(self, condition):
         debug('followPipeUntil: %s', condition.is_set())
@@ -153,12 +166,12 @@ class script(aiScript):
             self.auv.prop(0)
             # check we're still good to go, giving a little time to re-align the 
             # pipe if neededx
-            info("Re-aligning with pipe...")
+            debug("Re-aligning with pipe...")
             self.ready.wait(self.options.lost_timeout)
             if not self.ready.is_set():
                 return False
             
-            info("Above pipe, heading forward...")
+            debug("Above pipe, heading forward...")
             self.auv.prop(self.options.prop_speed)
             
             # go forward for a bit while we're still above the pipe
