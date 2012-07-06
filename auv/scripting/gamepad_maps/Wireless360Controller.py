0774d3bba28f172be2bebd5f457c16278c617880
Jason Wyatt <jw682@cam.ac.uk>
2012-07-06 09:28:42 +0200

    hopefully gamepad works on mac and linux now

--- auv/scripting/gamepad_maps/Wireless360Controller.py
+++ auv/scripting/gamepad_maps/Wireless360Controller.py
@@ -1,8 +1,3 @@
-import cauv.messaging as messaging
-from cauv.debug import debug, error, warning, info
-
-from gamepad_maps.GamepadMapping import *
-
 class XBoxButtons:
     UP = 0
     DOWN = 1
@@ -27,34 +22,3 @@ class XBoxAxes:
     JOY_R_Y = 3
     LT = 4
     RT = 5
-
-class ConcreteGamepadMapping(GamepadMapping):
-    def __init__(self, auv):
-        GamepadMapping.__init__(self, auv)
-    
-    def buttonPressed(self, button):
-        if button == XBoxButtons.A:
-            info("Running prop")
-            self.auv.prop(127)
-        if button == XBoxButtons.X:
-            info("Stop!")
-            self.auv.stop()
-    
-    def buttonReleased(self, button):
-        if button == XBoxButtons.A:
-            info("Stopping prop")
-            self.auv.prop(0)
-
-    def handleAxis(self, axisId, value):
-        if axisId == XBoxAxes.JOY_R_X:
-            self.bearing = (self.bearing + (2 * value * value * value))%360
-            self.auv.bearing(self.bearing)
-        
-        if axisId == XBoxAxes.JOY_R_Y:
-            self.pitch = ((self.pitch + (value * value * value) + 180)%360) - 180
-            self.auv.pitch(self.pitch)
-
-    def axisNeutralValue(self, axisId):
-        if axisId == 4 or axisId == 5:
-            return -1.0
-        return 0.0
