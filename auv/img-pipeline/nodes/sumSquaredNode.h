574989dfaf1537b3bb451d8373a30c146b591b2a
Leszek Swirski <ls426@cam.ac.uk>
2012-07-06 23:29:53 +0200

    Added sum squared node

---
+++ auv/img-pipeline/nodes/sumSquaredNode.h
@@ -0,0 +1,75 @@
+/* Copyright 2011-2012 Cambridge Hydronautics Ltd.
+ *
+ * Cambridge Hydronautics Ltd. licenses this software to the CAUV student
+ * society for all purposes other than publication of this source code.
+ * 
+ * See license.txt for details.
+ * 
+ * Please direct queries to the officers of Cambridge Hydronautics:
+ *     James Crosby    james@camhydro.co.uk
+ *     Andy Pritchard   andy@camhydro.co.uk
+ *     Leszek Swirski leszek@camhydro.co.uk
+ *     Hugo Vincent     hugo@camhydro.co.uk
+ */
+
+#ifndef __SUMSQUARED_NODE_H__
+#define __SUMSQUARED_NODE_H__
+
+#include <boost/bind.hpp>
+
+#include <opencv2/core/core.hpp>
+
+#include "../node.h"
+#include "../nodeFactory.h"
+#include "../pipelineTypes.h"
+
+
+namespace cauv{
+namespace imgproc{
+
+class SumSquaredNode: public Node{
+    public:
+        SumSquaredNode(ConstructArgs const& args)
+            : Node(args){
+        }
+
+        void init(){
+            // fast node:
+            m_speed = fast;
+
+            // inputs:
+            registerInputID("image");
+
+            // one output
+            registerOutputID<float>("sum sq", 0.0f);
+        }
+
+    protected:
+
+        static float sumsquared(const cv::Mat& m) {
+            if (m.channels() != 1)
+                throw parameter_error("Input matrix has to be single channel");
+
+            float sumsq = 0;
+            cv::MatConstIterator_<unsigned char> it, itend;
+            for (it = m.begin<unsigned char>(), itend = m.end<unsigned char>(); it != itend; ++it)
+                sumsq += *it/255.0f;
+            
+            return sumsq / m.total();
+        }
+
+        void doWork(in_image_map_t& inputs, out_map_t& r){
+
+            image_ptr_t img = inputs["image"];
+            r["sum sq"] = img->apply(boost::bind(sumsquared, _1));
+            
+        }
+    
+    // Register this node type
+    DECLARE_NFR;
+};
+
+} // namespace imgproc
+} // namespace cauv
+
+#endif // ndef __SUMSQUARED_NODE_H__
