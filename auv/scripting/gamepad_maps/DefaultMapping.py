bb36bd346c89a71a3ed1a897fcbcddfb8c6379ef
Jason Wyatt <jw682@cam.ac.uk>
2012-07-07 10:23:34 +0200

    woops, forgot more files from gamepad stuff

---
+++ auv/scripting/gamepad_maps/DefaultMapping.py
@@ -0,0 +1,124 @@
+import cauv.messaging as messaging
+from cauv.debug import debug, error, warning, info
+
+from gamepad_maps.GamepadMapping import *
+
+class ConcreteGamepadMapping(GamepadMapping):
+    def __init__(self, auv, xbox_axes, xbox_buttons):
+        GamepadMapping.__init__(self, auv)
+        self.Locked = True
+        self.JOY_RPressed = False
+        self.auv.depth_disabled = False
+        self.xbox_axes = xbox_axes
+        self.xbox_buttons = xbox_buttons
+        
+    def controls(self):
+        return """
+        While holding right trigger (Unlock):
+            Left Stick: Prop (Up/Down), Strafe (Left/Right).
+            Right Stick: Pitch(Up/Down), Turn (Left/Right), Set Bearing (Left/Right while holding stick in).
+            D-Pad: Depth/Bearing in 0.1 m/5 degree increments.
+        
+        X turns everything off.
+        """
+    
+    def buttonPressed(self, button):
+        #if button == self.xbox_buttons.A:
+        #    info("Running prop")
+        #    self.auv.prop(127)
+        if button == self.xbox_buttons.X:
+            info("Stop!")
+            self.auv.stop()
+            self.bearing_state = False
+            self.pitch_state = False
+            self.depth_state = False
+            
+        #all the following controls are default locked
+        if self.Locked:
+            return
+            
+        #Fine control of autopilots
+        if button == self.xbox_buttons.LEFT:
+            #auto pilot mode check first
+            if self.bearing_state == False:
+                self.bearing = self.auv.current_bearing
+                self.bearing_state = True
+            self.bearing = (self.bearing - 5)%360
+            self.auv.bearing(self.bearing)
+        if button == self.xbox_buttons.RIGHT:
+            #auto pilot mode check first
+            if self.bearing_state == False:
+                self.bearing = self.auv.current_bearing
+                self.bearing_state = True
+            self.bearing = (self.bearing + 5)%360
+            self.auv.bearing(self.bearing)
+        if button == self.xbox_buttons.UP:
+            #auto pilot mode check first
+            if self.depth_state == False:
+                self.depth = 0
+                self.depth_state = True
+            self.depth = self.depth - 0.1
+            self.auv.depth(self.depth)
+        if button == self.xbox_buttons.DOWN:
+            #auto pilot mode check first
+            if self.depth_state == False:
+                self.depth = 0
+                self.depth_state = True
+            self.depth = self.depth + 0.1
+            self.auv.depth(self.depth)
+                        
+        if button == self.xbox_buttons.JOY_R:
+            self.JOY_RPressed = True
+        
+    def buttonReleased(self, button):
+        if button == self.xbox_buttons.JOY_R:
+            self.JOY_RPressed = False
+        
+    def handleAxis(self, axisId, value):
+        #all the following controls are default locked
+        if axisId == self.xbox_axes.RT:
+            if value > 0.5:
+                self.Locked = False
+            else:
+                self.Locked = True
+                self.auv.strafe(0)
+                self.auv.prop(0)
+                if self.depth_state:
+                    self.auv.depth(self.depth)
+                if self.bearing_state:
+                    self.auv.bearing(self.bearing)
+                if self.pitch_state:
+                    self.auv.pitch(self.pitch)
+        
+        if self.Locked:
+            return
+        if axisId == self.xbox_axes.JOY_R_X:
+            #auto pilot mode check first
+            if not self.bearing_state:
+                self.bearing = self.auv.current_bearing
+                self.bearing_state = True
+            if value == 0 and not self.JOY_RPressed:
+                self.bearing = self.auv.current_bearing
+                self.auv.bearing(self.bearing)
+            else:
+                self.bearing = (self.bearing + (2 * value * value * value))%360
+                self.auv.bearing(self.bearing)
+        
+        if axisId == self.xbox_axes.JOY_R_Y:
+            #auto pilot mode check first
+            if not self.pitch_state:
+                self.pitch = 0
+                self.pitch_state = True
+            self.pitch = ((self.pitch + (value * value * value) + 180)%360) - 180
+            self.auv.pitch(self.pitch)
+
+        if axisId == self.xbox_axes.JOY_L_X:
+            self.auv.strafe(-int(value*127))
+        
+        if axisId == self.xbox_axes.JOY_L_Y:
+            self.auv.prop(-int(value*127))
+            
+    def axisNeutralValue(self, axisId):
+        if axisId == self.xbox_axes.LT or axisId == self.xbox_axes.RT:
+            return -1.0
+        return 0.0
