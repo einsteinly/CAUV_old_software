6c34f29d50b828d2ffd3d6ad906e98bebc25b501
Jason Wyatt <jw682@cam.ac.uk>
2012-07-07 13:22:32 +0200

    fixed colour node

--- auv/img-pipeline/nodes/math/mathNodes.cpp
+++ auv/img-pipeline/nodes/math/mathNodes.cpp
@@ -29,6 +29,7 @@ template<> DEFINE_NFR(MathDivideNode<int>, NodeType::MathDivideInt);
 template<> DEFINE_NFR(MathDivideNode<float>, NodeType::MathDivideFloat);
 template<> DEFINE_NFR(ClampNode<int>, NodeType::ClampInt);
 template<> DEFINE_NFR(ClampNode<float>, NodeType::ClampFloat);
+template<> DEFINE_NFR(ClampNode<Colour>, NodeType::ClampColour);
 template<> DEFINE_NFR(ValueInputNode<int32_t>, NodeType::IntInput);
 template<> DEFINE_NFR(ValueInputNode<float>, NodeType::FloatInput);
 template<> DEFINE_NFR(ValueInputNode<bool>, NodeType::BoolInput);
