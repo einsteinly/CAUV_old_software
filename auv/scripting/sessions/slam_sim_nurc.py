a3160179c8f3e9d1d0a7ed4603347d9b067c0953
James Crosby <james.crosby@camhydro.co.uk>
2012-07-07 04:52:19 +0200

    fix fix fix

--- auv/scripting/sessions/slam_sim_nurc.py
+++ auv/scripting/sessions/slam_sim_nurc.py
@@ -2,22 +2,22 @@ from utils.watchfuncs import *
 
 processes = [
     Process('control',     '{BDIR}', node_pid('Control'),  restart(), None,
-        ['{BDIR}/auv/bin/control{D} -N']
+        ['{BDIR}/control{D} -N']
     ),
     Process('sim.py',      '{SDIR}', node_pid('py-sim'),   panic,     None,
-        ["sh -c 'sleep 2 && PYTHONPATH=$PYTHONPATH:{SDIR} python2.7 {SDIR}/simulator/sim.py'"]
+        ["sh -c 'sleep 2 && {SDIR}/run.sh {SDIR}/simulator/sim.py'"]
     ),
     Process('img-pipeline','{BDIR}', node_pid('img-pipe'), panic,     None,
-        ['{BDIR}/auv/bin/img-pipeline']
+        ['{BDIR}/img-pipeline']
     ),
-    Process('savepl.py',   '{SDIR}', node_pid('py-play'),  None,      None,
-        ["sh -c 'sleep 2 && {SDIR}/run.sh ./savepl.py {SDIR}/pipelines/slam.pipe load'"]
+    Process('savepl.py',   '{SDIR}', node_pid('py-play'),  panic,      None,
+        ["sh -c 'sleep 2 && {SDIR}/run.sh {SDIR}/savepl.py -f {SDIR}/pipelines/slam.pipe load'"]
     ),
     Process('persist',     '{SDIR}', node_pid('persist'),  restart(), None,
         ['{SDIR}/persist.py -rs']
     ),
     Process('fake-gemini', '{BDIR}', node_pid('FakeGem'),  panic,     None,
-        ["sh -c 'sleep 6 && {BDIR}/auv/bin/fake_gemini -e {SDIR}/tests/resources/comp_arena_2d.png -s 0.038 -x -780 -y -400'"]
+        ["sh -c 'sleep 6 && {BDIR}/fake_gemini -e {SDIR}/tests/resources/comp_arena_2d.png -s 0.038 -x -780 -y -400'"]
     ),
 ]
 
