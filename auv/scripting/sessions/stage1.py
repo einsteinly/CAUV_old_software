481d2f44be931a92439a1f412af797d3f8a1f280
James Crosby <james.crosby@camhydro.co.uk>
2012-07-10 15:37:55 +0200

    fix stage1 script

--- auv/scripting/sessions/stage1.py
+++ auv/scripting/sessions/stage1.py
@@ -10,18 +10,17 @@ common_processes = [
 if socket.gethostname() == 'barracuda-seco':
     # processes running on seco only
     processes = common_processes + [
-        Process('bridge',     '/',       search_pid('mcb_bridge'), restart(), 'root', ['{BDIR}/mcb_bridge -p /dev/ttyS0 -n 3 --prefix /tmp/cauv/ports/ttyV'])
+        Process('bridge',     '/',       search_pid('mcb_bridge'), restart(), 'root', ['{BDIR}/mcb_bridge -p /dev/ttyS0 -n 3 --prefix /tmp/cauv/ports/ttyV']),
         Process('control',    '/',       node_pid('control'),      restart(), 'root', ['{BDIR}/control{D} --sbg /tmp/cauv/ports/ttyV1 --imu sbg --mcb barracuda --port /tmp/cauv/ports/ttyVCAN']),
         Process('persist',    '{SDIR}',  node_pid('persist'),      restart(),  None,  ['{SDIR}/persist.py -rs -f {SDIR}/persist/sim']),
         Process('daemon man', '{SDIR}',  node_pid('daemon_man'),   restart(),  None,  ['{SDIR}/daemon_man -peer 10.0.0.3']),
         Process('last resort','{SDIR}', search_pid('last-resort.py'),restart(),None,  ['{SDIR}/last-resort 7777 300']),
     ]
-elif socket.gethostname() == 'barracuda-arm':
+else:
     # processes running on the panda board only
     processes = common_processes + [
-        Process('daemon_man'  '{SDIR}',  node_pid('daemon_man'),   restart(),  None,  ['{SDIR}/daemon_man -peer 10.0.0.2']),
+        Process('daemon_man',  '{SDIR}',  node_pid('daemon_man'),   restart(),  None,  ['{SDIR}/daemon_man -peer 10.0.0.2']),
     ]
-
 
 def get_processes(args):
     return processes
