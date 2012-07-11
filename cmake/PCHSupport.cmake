ba185623933509d7bbcae30b018fc4caf1e02d46
Leszek Swirski <ls426@cam.ac.uk>
2012-07-11 13:12:49 +0200

    Minor pch header fix

--- cmake/PCHSupport.cmake
+++ cmake/PCHSupport.cmake
@@ -36,40 +36,6 @@ else()
     message(STATUS "PCH support not found")
 endif()
 
-MACRO(_PCH_GET_COMPILE_FLAGS _out_compile_flags)
-
-    STRING(TOUPPER "CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE}" _flags_var_name)
-    SET(${_out_compile_flags} ${${_flags_var_name}} )
-
-    IF(CMAKE_COMPILER_IS_GNUCXX)
-
-        GET_TARGET_PROPERTY(_targetType ${_PCH_current_target} TYPE)
-        IF(${_targetType} STREQUAL SHARED_LIBRARY AND NOT WIN32)
-            LIST(APPEND ${_out_compile_flags} "-fPIC")
-        ENDIF()
-
-    ELSE()
-        ## TODO ... ? or does it work out of the box
-    ENDIF()
-
-    GET_DIRECTORY_PROPERTY(DIRINC INCLUDE_DIRECTORIES )
-    FOREACH(item ${DIRINC})
-        LIST(APPEND ${_out_compile_flags} "${_PCH_isystem_prefix}" "${item}")
-    ENDFOREACH()
-
-    GET_DIRECTORY_PROPERTY(_directory_flags DEFINITIONS)
-    #MESSAGE("_directory_flags ${_directory_flags} ${_global_definitions}" )
-    LIST(APPEND ${_out_compile_flags} ${_directory_flags})
-    LIST(APPEND ${_out_compile_flags} ${CMAKE_CXX_FLAGS})
-
-    SEPARATE_ARGUMENTS(${_out_compile_flags})
-
-ENDMACRO(_PCH_GET_COMPILE_FLAGS)
-
-
-macro(_pch_write_pchdep_cxx _dephelp_cpp _dephelp _include_file)
-
-endmacro()
 
 
 macro(_pch_get_compile_command out_command _input _output)
@@ -181,8 +147,6 @@ macro(add_precompiled_header _target _header_file)
 
         set(_dephelp "${_pch_target}_dephelp")
 
-
-        _pch_get_compile_flags(_compile_FLAGS)
         _pch_get_compile_command(_command  ${_header_file} ${_output} )
         
         file(COPY "${_header_file}" DESTINATION "${_outdir}")
