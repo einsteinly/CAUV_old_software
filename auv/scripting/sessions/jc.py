f2c5c057491bdcf71c56a7d93c9a42eb8dcad99e
James Crosby <james.crosby@camhydro.co.uk>
2012-07-11 16:30:48 +0200

    keepalive for wifi

--- auv/scripting/sessions/jc.py
+++ auv/scripting/sessions/jc.py
@@ -7,7 +7,8 @@ processes = [
     Process('heimdall',   '{BDIR}',  zmq_daemon_pid(),       panic,      None,  ['{BDIR}/heimdall{D} -s']), 
     Process('daemon man', '{SDIR}',  node_pid('daemon_man'), restart(),  None,  ["sh -c 'PYTHONPATH=$PYTHONPATH:{SDIR} {SDIR}/run.sh {SDIR}/daemon_man.py --peer 10.0.0.2'"]),
     Process('scratch',    '{BDIR}',  node_pid('scratch'),    restart(),  None,  ['{BDIR}/scratch -n default/video']),
-    Process('keepalive',  '{BDIR}',  search_pid('telnet'),   restart(),  None,  ['telnet 10.0.0.2 7777']),
+    Process('keepalive-wifi', '{BDIR}',  search_pid('telnet 10.0.0.4'),  restart(),  None,  ['telnet 10.0.0.4 7777']),
+    Process('keepalive-eth',  '{BDIR}',  search_pid('telnet 10.0.0.2'),  restart(),  None,  ['telnet 10.0.0.2 7777']),
 ]
 
 def get_processes(args):
