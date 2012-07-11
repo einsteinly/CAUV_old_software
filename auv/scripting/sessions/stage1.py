9617cf79bba825a6696653d8b0a28e3bc26e72e8
Barracuda <info@cambridgeauv.co.uk>
2012-07-11 10:58:45 +0100

    delay before control starts

--- auv/scripting/sessions/stage1.py
+++ auv/scripting/sessions/stage1.py
@@ -11,7 +11,7 @@ if True or socket.gethostname() == 'barracuda-seco':
     # processes running on seco only
     processes = common_processes + [
         Process('bridge',     '/',       search_pid('mcb_bridge'), restart(), 'root', ['{BDIR}/mcb_bridge -p /dev/ttyS0 -n 3 --prefix /tmp/cauv/ports/ttyV']),
-        Process('control',    '/',       node_pid('control'),      restart(), 'root', ['{BDIR}/control{D} --sbg /tmp/cauv/ports/ttyV1 --imu sbg --mcb barracuda --port /tmp/cauv/ports/ttyVCAN']),
+        Process('control',    '/',       node_pid('control'),      restart(), 'root', ["sh -c 'sleep 2.0 && {BDIR}/control{D} --sbg /tmp/cauv/ports/ttyV1 --imu sbg --mcb barracuda --port /tmp/cauv/ports/ttyVCAN'"]),
         Process('persist',    '{SDIR}',  node_pid('persist'),      restart(),  None,  ['{SDIR}/persist.py -rs -f {SDIR}/persist/sim']),
         Process('daemon man', '{SDIR}',  node_pid('daemon_man'),   restart(),  None,  ['{SDIR}/daemon_man.py --peer 10.0.0.3']),
         Process('last resort','{SDIR}', search_pid('last-resort.py'),restart(),None,  ['{SDIR}/last-resort.py 7777 300']),
