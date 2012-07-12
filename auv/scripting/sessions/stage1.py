8848c6abf572a742178c700b7ee0b0fbd8f55556
Douglas Young <dy245@cam.ac.uk>
2012-07-12 02:42:57 +0100

    Minor fix to stage1.py to reflect changes to mcb_bridge

--- auv/scripting/sessions/stage1.py
+++ auv/scripting/sessions/stage1.py
@@ -11,7 +11,7 @@ if True or socket.gethostname() == 'barracuda-seco':
     # processes running on seco only
     processes = common_processes + [
         Process('bridge',     '/',       search_pid('mcb_bridge'), restart(),  None, ['{BDIR}/mcb_bridge -p /dev/ttyS0 -n 3 --prefix /tmp/ttyV']),
-        Process('control',    '/',       node_pid('control'),      restart(),  None, ["sh -c 'sleep 1.0 && {BDIR}/control{D} --sbg /tmp/ttyV1 --imu sbg --mcb barracuda --port /tmp/ttyVCAN'"]),
+        Process('control',    '/',       node_pid('control'),      restart(),  None, ["sh -c 'sleep 1.0 && {BDIR}/control{D} --sbg /tmp/ttyV1 --imu sbg --mcb barracuda --port /tmp/ttyVCAN0'"]),
         Process('persist',    '{SDIR}',  node_pid('persist'),      restart(),  None, ['{SDIR}/persist.py -rs -f {SDIR}/persist/sim']),
         Process('daemon_man', '{SDIR}',  node_pid('daemon-man'),   restart(),  None, ['{SDIR}/daemon_man.py --peer 10.0.0.3']),
         Process('last resort','{SDIR}', search_pid('last-resort.py'),restart(),None, ['{SDIR}/last-resort.py 7777 300']),
