d72dc752d663c44a533a7dfa81baa4f68665e9a7
Jason Wyatt <jw682@cam.ac.uk>
2012-12-03 19:58:54 +0000

    fixed using old version of AI

--- auv/scripting/sessions/ai_procs.py
+++ auv/scripting/sessions/ai_procs.py
@@ -1,11 +1,11 @@
 from utils.watchfuncs import *
 
 barracuda_processes = [
-    Process('pipeline_manager',    ['AI_pipeline_manager.py'], node_pid('ai_pl_manager'), death = restart()),
-    Process('auv_control', ['AI_control_manager.py'], node_pid('ai_auv_control'), death = restart()),
-    Process('detector_control', ['AI_detection_process.py'], node_pid('ai_detector_control'), death = restart()),
-    Process('location', ['AI_location.py'], node_pid('ai_location')),
-    Process('task_manager', ['AI_task_manager.py'], node_pid('ai_task_manager'), death = restart(), prereq = depends_on('pipeline_manager', 'auv_control', 'detector_control', 'location')),
+    Process('pipeline_manager',    ['pipeline_manager.py'], node_pid('ai_pl_manager'), death = restart()),
+    Process('auv_control', ['control_manager.py'], node_pid('ai_auv_control'), death = restart()),
+    Process('detector_control', ['detection_process.py'], node_pid('ai_detector_control'), death = restart()),
+    Process('location', ['location.py'], node_pid('ai_location')),
+    Process('task_manager', ['task_manager.py'], node_pid('ai_task_manager'), death = restart(), prereq = depends_on('pipeline_manager', 'auv_control', 'detector_control', 'location')),
 ]
 
 def get_processes(args):
