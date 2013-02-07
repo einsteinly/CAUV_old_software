c7aa6c604ec009131ffa76fc1cf2d9a52b2da1a4
James Buckley <xanium4332@gmail.com>
2013-02-07 16:22:31 +0000

    Spring clean: removed old mcb dead code

--- auv/embedded/legacy-mcb/src/common/utils/bytes_to_float.h
+++
@@ -1,7 +0,0 @@
-#ifndef BYTES_TO_FLOAT
-#define BYTES_TO_FLOAT
-
-inline float bytesToFloat(char byte1,char byte2,char byte3,char byte4);
-
-#endif /*BYTES_TO_FLOAT */
-
--- auv/embedded/legacy-mcb/src/common/utils/bytes_to_long.h
+++
@@ -1,7 +0,0 @@
-#ifndef BYTES_TO_LONG
-#define BYTES_TO_LONG
-
-inline long bytesToLong(char byte1, char byte2, char byte3, char byte4);
-
-#endif /* BYTES_TO_LONG */
-
