2c41f24dc67e156aef1db509308389dd19ff92b5
Leszek Swirski <ls426@cam.ac.uk>
2013-06-28 16:35:59 +0200

    One more try...

--- auv/img-pipeline/nodes/fileInputNode.h
+++ auv/img-pipeline/nodes/fileInputNode.h
@@ -24,17 +24,44 @@
 #define WORKAROUND_CV_CAPPROP
 #warning Workaround for missing CAP_PROP_FRAME_WIDTH/HEIGHT in OpenCV <= 2.4.5
 namespace cv {
-    enum
-    {
-        CV_FFMPEG_CAP_PROP_POS_MSEC=0,
-        CV_FFMPEG_CAP_PROP_POS_FRAMES=1,
-        CV_FFMPEG_CAP_PROP_POS_AVI_RATIO=2,
-        CV_FFMPEG_CAP_PROP_FRAME_WIDTH=3,
-        CV_FFMPEG_CAP_PROP_FRAME_HEIGHT=4,
-        CV_FFMPEG_CAP_PROP_FPS=5,
-        CV_FFMPEG_CAP_PROP_FOURCC=6,
-        CV_FFMPEG_CAP_PROP_FRAME_COUNT=7
-    };
+    enum { CAP_PROP_POS_MSEC       =0,
+           CAP_PROP_POS_FRAMES     =1,
+           CAP_PROP_POS_AVI_RATIO  =2,
+           CAP_PROP_FRAME_WIDTH    =3,
+           CAP_PROP_FRAME_HEIGHT   =4,
+           CAP_PROP_FPS            =5,
+           CAP_PROP_FOURCC         =6,
+           CAP_PROP_FRAME_COUNT    =7,
+           CAP_PROP_FORMAT         =8,
+           CAP_PROP_MODE           =9,
+           CAP_PROP_BRIGHTNESS    =10,
+           CAP_PROP_CONTRAST      =11,
+           CAP_PROP_SATURATION    =12,
+           CAP_PROP_HUE           =13,
+           CAP_PROP_GAIN          =14,
+           CAP_PROP_EXPOSURE      =15,
+           CAP_PROP_CONVERT_RGB   =16,
+           CAP_PROP_WHITE_BALANCE_BLUE_U =17,
+           CAP_PROP_RECTIFICATION =18,
+           CAP_PROP_MONOCROME     =19,
+           CAP_PROP_SHARPNESS     =20,
+           CAP_PROP_AUTO_EXPOSURE =21, // DC1394: exposure control done by camera, user can adjust refernce level using this feature
+           CAP_PROP_GAMMA         =22,
+           CAP_PROP_TEMPERATURE   =23,
+           CAP_PROP_TRIGGER       =24,
+           CAP_PROP_TRIGGER_DELAY =25,
+           CAP_PROP_WHITE_BALANCE_RED_V =26,
+           CAP_PROP_ZOOM          =27,
+           CAP_PROP_FOCUS         =28,
+           CAP_PROP_GUID          =29,
+           CAP_PROP_ISO_SPEED     =30,
+           CAP_PROP_BACKLIGHT     =32,
+           CAP_PROP_PAN           =33,
+           CAP_PROP_TILT          =34,
+           CAP_PROP_ROLL          =35,
+           CAP_PROP_IRIS          =36,
+           CAP_PROP_SETTINGS      =37
+         };
 }
 #endif
 #endif
