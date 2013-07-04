c3b97feb70daf366434bef7bec00e03cad515fc5
Jason Wyatt <jw682@cam.ac.uk>
2013-07-05 01:14:07 +0200

    Fixes to simple wall following

--- auv/scripting/script_library/track_wall_simple.py
+++ auv/scripting/script_library/track_wall_simple.py
@@ -6,6 +6,7 @@ from cauv.debug import debug, info, warning, error
 import AI
 from utils.control import PIDController
 from utils.coordinates import Simulation_Datum, NorthEastDepthCoord
+from utils.boundedtypes import MotorValue
 
 import time
 import math
@@ -20,8 +21,8 @@ class TrackWallSimple(AI.Script):
     class DefaultOptions(AI.Script.DefaultOptions):
         def __init__(self):
             self.strafekP = 1000 #controls strafe speed, int [-127, 127]
-            self.strafeLimit = AI.OptionWithMeta(60, type=MotorValue)
-            self.strafeOffset = AI.OptionWithMeta(10, type=MotorValue)
+            self.strafeLimit = AI.OptionWithMeta(60, opt_type=MotorValue)
+            self.strafeOffset = AI.OptionWithMeta(10, opt_type=MotorValue)
             self.wallDistancekP = -1000
             self.depth = AI.OptionWithMeta(2, units="m") #depth in metres
             self.useDepth = True
@@ -29,10 +30,10 @@ class TrackWallSimple(AI.Script):
             self.targetDistance = 0.1
             self.controlBearing = False
             self.initialLocation = (Simulation_Datum+NorthEastDepthCoord(-18, 5, 0)).toWGS84()
-            self.initialBearing = 80
+            self.initialBearing = 180
             self.perpendicularError = 5
             self.perpendicularRationLimit = 0.1
-            self.linesName = 'track_wall'
+            self.linesName = 'wall_lines'
             self.pipeline = 'track_wall2'
     
     class Debug(AI.Script.Debug):
@@ -41,6 +42,7 @@ class TrackWallSimple(AI.Script):
             self.distance = 0
             
     def __init__(self):
+        AI.Script.__init__(self)
         self.angle = 0
         self.distance = 0
         
@@ -51,8 +53,9 @@ class TrackWallSimple(AI.Script):
 !!!! Write help here !!!!
 
         """
-        angle, distance = findBestLine(m.lines)
-        perpendicularRatio = findPerpendicularPercent();
+        angle, distance = self.findBestLine(m.lines)
+        perpendicularRatio = self.findPerpendicularRatio(m.lines);
+        angle = math.degrees(angle)
         
         #set debug values
         self.angle = angle
@@ -61,51 +64,51 @@ class TrackWallSimple(AI.Script):
         
         #Prop Control
         speed = 0
-        if perpendicularRatio < self.perpendicularRationLimit
+        if perpendicularRatio < self.options.perpendicularRationLimit:
             speed = (0.5+self.options.targetDistance-distance)*self.options.wallDistancekP+self.options.strafeOffset
         debug('Setting speed %f' %(speed))
         self.auv.prop(int(speed))
         
         #Bearing Control
-        if self.controlBearing
+        if self.options.controlBearing:
             debug('Setting bearing %f' %(self.auv.current_bearing+angle))
             self.auv.bearing(self.auv.current_bearing+angle)
             
         #Strafe Control
         #don't strafe when to far from the wall because the AUV won't see the wall with its cameras
         strafe_speed = 0
-        if perpendicularRatio < self.perpendicularRationLimit
+        if perpendicularRatio < self.options.perpendicularRationLimit:
             strafe_speed = (0.5+self.options.targetDistance-distance)*self.options.strafekP
             strafe_speed = self.options.strafeLimit if strafe_speed > self.options.strafeLimit else strafe_speed
             strafe_speed = -self.options.strafeLimit if strafe_speed < -self.options.strafeLimit else strafe_speed
         self.auv.strafe(int(strafe_speed))
         debug('Setting strafe %f' %(strafe_speed))
     
-    def findBestLine(self, lines)
+    def findBestLine(self, lines):
         #Just take the weighted(based on line length) average of everything
         length = 0
         angle = 0
         distance = 0
-        for line in lines
+        for line in lines:
             #Find distance from the current position to the line.
             #direction of the line
             #IS THE ANGLE IN RADIANS OR DEGREES HERE?
             t = msg.floatXY(math.cos(line.angle), math.sin(line.angle))
             #a vector from the AUV to any point of the line
             a = msg.floatXY(line.centre.x - 0.5, line.centre.y - 0.5)
-            distance += math.abs(t.x*a.y - t.y*a.x)*line.length
+            distance += abs(t.x*a.y - t.y*a.x)*line.length
             angle += line.angle*line.length
             length += line.length
         return angle/length, distance/length;     
         
-    def findPerpendicularRatio(self, lines)
+    def findPerpendicularRatio(self, lines):
         totalPairLength = 0
         perpedicularLength = 0
-        for i in range(len(lines))
-            for j in range(i:len(lines))
-                line1 = line[i]
-                line2 = line[j]
-                if math.abs(line1.angle-line2.angle) < self.perpendicularError
+        for i in range(len(lines)):
+            for j in range(i,len(lines)):
+                line1 = lines[i]
+                line2 = lines[j]
+                if abs(math.degrees(line1.angle-line2.angle)) < self.options.perpendicularError:
                     perpedicularLength += line1.length + line2.length
                 totalPairLength += line1.length + line2.length
                     
